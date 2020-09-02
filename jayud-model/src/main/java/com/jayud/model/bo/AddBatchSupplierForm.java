8a2e2d78f55d534a3edeb9e80da6ed29cc37161c
jyd2018 <Jayud@)@)08>
2020-09-02 09:54:41 +0800

    整理项目

---
+++ jayud-model/src/main/java/com/jayud/model/bo/AddBatchSupplierForm.java
@@ -0,0 +1,15 @@
+package com.jayud.model.bo;
+
+import lombok.Data;
+
+/**
+ * 批量确认新增供应商
+ *
+ * @author william
+ * @description
+ * @Date: 2020-08-03 09:23
+ */
+@Data
+public class AddBatchSupplierForm {
+
+}
---
+++ jayud-model/src/main/java/com/jayud/model/vo/CarrierSupplierDetailVO.java
@@ -0,0 +1,15 @@
+package com.jayud.model.vo;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author william
+ * @description
+ * @Date: 2020-07-22 14:25
+ */
+@Data
+public class CarrierSupplierDetailVO {
+
+}

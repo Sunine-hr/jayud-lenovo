c181ce83a0b446538d1439dcedbb02e3a2b9aeec
zcm2012888 <2373329941@qq.com>
2020-09-05 22:38:02 +0800

    Swagger

--- jayud-common/src/main/java/com/jayud/common/config/Swagger2Config.java
+++ jayud-common/src/main/java/com/jayud/common/config/Swagger2Config.java
@@ -1,9 +1,15 @@
-package com.jayud.oms;
+package com.jayud.common.config;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -12,7 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @Configuration
 @ConditionalOnExpression("${swagger.enable:true}")
-public class Swagger2 {
+public class Swagger2Config {
     /**
      * 创建API应用
      * apiInfo() 增加API相关信息
@@ -21,17 +27,17 @@ public class Swagger2 {
      *
      * @return
      */
-   /* @Bean
+   @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.lenovo."))
+                .apis(RequestHandlerSelectors.basePackage("com.jayud"))
                 //只显示加了注解的api
                 .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
                 .paths(PathSelectors.any())
                 .build();
-    }*/
+    }
 
     /**
      * 创建该API的基本信息（这些基本信息会展现在文档页面中）

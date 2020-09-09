c43ef26f1b1d0d191765afd06803da8154ee4b2b
zcm2012888 <2373329941@qq.com>
2020-09-09 11:08:09 +0800

    法人主体审核

--- jayud-oauth/jayud-oauth-model/src/main/java/com/jayud/model/bo/AuditLegalEntityForm.java
+++ jayud-oauth/jayud-oauth-model/src/main/java/com/jayud/model/bo/AuditLegalEntityForm.java
@@ -9,6 +9,6 @@ public class AuditLegalEntityForm {
     @ApiModelProperty(value = "法人主体ID",required = true)
     private Long id;
 
-    @ApiModelProperty(value = "审核状态 1-待审核 2-通过 3-拒绝",required = true)
+    @ApiModelProperty(value = "审核状态 1-待审核 2-通过 0-拒绝",required = true)
     private Long auditStatus;
 }

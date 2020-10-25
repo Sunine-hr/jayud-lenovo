6875c421ab8a809ce719c97b377847ce4c39ef2c
mfcfine <mfcfine@163.com>
2020-10-25 16:34:10 +0800

    南京电商后台

--- jayud-mall/jayud-mall-service/src/test/java/Test2.java
+++ jayud-mall/jayud-mall-service/src/test/java/Test2.java
@@ -7,17 +7,18 @@ public class Test2 {
 
     public static void main(String[] args) {
 
+        //不同数据类型的值比较是否相等
         Integer a = 0;
         long b = 0;
         if(a.equals(b)){
             System.out.println(true);
         }else{
-            System.out.println(false);
+            System.out.println(false);//为false
         }
         System.out.println("-----------");
 
         if(Long.valueOf(a).equals(b)){
-            System.out.println(true);
+            System.out.println(true);//为true
         }else{
             System.out.println(false);
         }

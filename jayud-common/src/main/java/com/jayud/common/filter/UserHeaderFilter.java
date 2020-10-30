a980be0ffa58aba6a76fb8b2708f6179b2dcdf67
zcm2012888 <2373329941@qq.com>
2020-10-30 11:51:54 +0800

    获取当前登录用户并延长redis保存token得时间

--- jayud-common/src/main/java/com/jayud/common/filter/UserHeaderFilter.java
+++ jayud-common/src/main/java/com/jayud/common/filter/UserHeaderFilter.java
@@ -9,6 +9,10 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 @WebFilter(urlPatterns = "/*", filterName = "userHeaderFilter")
@@ -17,17 +21,30 @@ public class UserHeaderFilter implements Filter {
     @Autowired
     RedisUtils redisUtils;
 
+    private static final Set<String> ALLOWED_PATHS = Collections.unmodifiableSet(new HashSet<>(
+            Arrays.asList("/system/user/login","/system/user/logout")));
+
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-        String token = httpServletRequest.getHeader("token");
-        String user = redisUtils.get(token,10000l);
-        UserOperator.setToken(user);
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        String path = request.getRequestURI().substring(request.getContextPath().length()).replaceAll("[/]+$", "");
+        boolean allowedPath = ALLOWED_PATHS.contains(path);
+        if (allowedPath) {
+            System.out.println("这里是不需要处理的url进入的方法");
+
+        }
+        else {
+            String token = request.getHeader("token");
+            String user = redisUtils.get(token,10000l);
+            UserOperator.setToken(user);
+        }
+        chain.doFilter(req, res);
     }
 
     @Override

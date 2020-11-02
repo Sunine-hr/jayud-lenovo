480b35253e50e32d09d1124ca4c2d659d79a04fb
李达荣 <365165904@qq.com>
2020-11-02 15:58:05 +0800

    工具更新

---
+++ jayud-common/src/main/java/com/jayud/common/utils/BeanUtils.java
@@ -0,0 +1,72 @@
+package com.jayud.common.utils;
+
+import com.jayud.common.func.SFunction;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.Serializable;
+import java.lang.invoke.SerializedLambda;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 实体工具类
+ */
+@Slf4j
+public class BeanUtils {
+
+    /**
+     * 存储对象属性
+     */
+    private static Map<Class, SerializedLambda> CLASS_LAMDBA_CACHE = new ConcurrentHashMap<>();
+
+
+    public static String[] convertToFieldName(SFunction... fns) {
+        String[] fields = new String[fns.length];
+        for (int i = 0; i < fns.length; i++) {
+            String str = convertToFieldName(fns[i]);
+            fields[i] = str;
+        }
+        return fields;
+    }
+
+    /**
+     * 转换方法引用为属性名
+     */
+    public static <T> String convertToFieldName(SFunction<T> fn) {
+        SerializedLambda lambda = getSerializedLambda(fn);
+        //获取方法名
+        String methodName = lambda.getImplMethodName();
+        String prefix = null;
+        if (methodName.startsWith("get")) {
+            prefix = "get";
+        } else if (methodName.startsWith("is")) {
+            prefix = "is";
+        }
+        if (prefix == null) {
+            log.error("无效的getter方法: " + methodName);
+        }
+        // 截取get/is之后的字符串并转换首字母为小写
+        return StringUtils.toLowerCaseFirstOne(methodName.replace(prefix, ""));
+    }
+
+    /**
+     * 获取方法（关键在于这个方法）
+     */
+    private static <T> SerializedLambda getSerializedLambda(Serializable fn) {
+        SerializedLambda lambda = CLASS_LAMDBA_CACHE.get(fn.getClass());
+        //先检查缓存中是已存在
+        if (lambda == null) {
+            try {
+                //提取SerializedLambda并缓存
+                Method method = fn.getClass().getDeclaredMethod("writeReplace");
+                method.setAccessible(true);
+                lambda = (SerializedLambda) method.invoke(fn);
+                CLASS_LAMDBA_CACHE.put(fn.getClass(), lambda);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return lambda;
+    }
+}

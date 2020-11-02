3ae09c5d2f10b0576893a2a2950292ab65b8188b
mfcfine <mfcfine@163.com>
2020-11-02 11:49:55 +0800

    南京电商后台 1.security bcryptPasswordEncoder自定义密码验证

--- jayud-mall/jayud-mall-admin/src/main/java/com/jayud/mall/security/service/UserDetailService.java
+++ jayud-mall/jayud-mall-admin/src/main/java/com/jayud/mall/security/service/UserDetailService.java
@@ -4,12 +4,14 @@ import com.jayud.mall.model.bo.SystemUserLoginForm;
 import com.jayud.mall.model.po.SystemRole;
 import com.jayud.mall.model.vo.SystemUserVO;
 import com.jayud.mall.security.domain.MyUser;
+import com.jayud.mall.security.utils.ContextHolderUtils;
 import com.jayud.mall.service.ISystemRoleService;
 import com.jayud.mall.service.ISystemUserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -17,8 +19,12 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -46,6 +52,10 @@ public class UserDetailService implements UserDetailsService {
      */
     @Autowired
     ISystemRoleService roleService;
+    /**
+     * <p>前端密码</p>
+     */
+    private String passwordParameter = "password";
 
     /**
      * 重写用户登录验证
@@ -55,32 +65,33 @@ public class UserDetailService implements UserDetailsService {
      */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        // 模拟一个用户，替代数据库获取逻辑
-//        MyUser user = new MyUser();
-//        user.setUserName(username);
-//        user.setPassword(this.passwordEncoder.encode("123456"));
-//        // 输出加密后的密码
-//        System.err.println(user.getPassword());
-//
-//        return new User(username, user.getPassword(), user.isEnabled(),
-//                user.isAccountNonExpired(), user.isCredentialsNonExpired(),
-//                user.isAccountNonLocked(), AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
         logger.debug("权限框架-加载用户");
+        HttpServletRequest request = ContextHolderUtils.getRequest();
+        String password = request.getParameter(passwordParameter);
+        logger.error("password = {}", password);
+
         List<GrantedAuthority> auths = new ArrayList<>();
 
         SystemUserLoginForm loginForm = new SystemUserLoginForm();
         //登录名（手机号／邮箱）不能为空
         loginForm.setLoginname(username);
         SystemUserVO userVO = userService.login(loginForm);
-
+        //用户认证
         if (userVO == null) {
-            logger.debug("找不到该用户 用户名:{}", username);
+            logger.debug("找不到该用户 （手机号／邮箱）:{}", username);
             throw new UsernameNotFoundException("找不到该用户！");
         }
+        //账号状态验证
         if(userVO.getStatus()==0) {
-            logger.debug("用户账号未启用，无法登陆 用户名:{}", username);
+            logger.debug("用户账号未启用，无法登陆 （手机号／邮箱）:{}", username);
             throw new UsernameNotFoundException("用户账号未启用！");
         }
+        // security bcryptPasswordEncoder自定义密码验证
+        BCryptPasswordEncoder bcryptPasswordEncoder = new BCryptPasswordEncoder();
+        if (!bcryptPasswordEncoder.matches(password,userVO.getPassword())){
+            throw new BadCredentialsException("密码错误，请重新输入");
+        }
+
         Long userId = userVO.getId();
         List<SystemRole> systemRoles = roleService.selectRolesByUserId(userId);
         if (systemRoles != null) {

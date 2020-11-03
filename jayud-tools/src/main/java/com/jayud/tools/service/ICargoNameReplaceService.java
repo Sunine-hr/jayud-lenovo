4809af63b0d0321964fd296d521339891ce9ba3d
mfcfine <mfcfine@163.com>
2020-11-03 19:21:47 +0800

    佳裕达小工具 1.完善货物名称替换表-基础接口 2.导出模板 3.导入 3.保存（新增、修改） 4.删除 5.查询list

--- jayud-tools/src/main/java/com/jayud/tools/service/ICargoNameReplaceService.java
+++ jayud-tools/src/main/java/com/jayud/tools/service/ICargoNameReplaceService.java
@@ -1,7 +1,11 @@
 package com.jayud.tools.service;
 
+import com.jayud.common.CommonResult;
+import com.jayud.tools.model.bo.CargoNameReplaceForm;
 import com.jayud.tools.model.po.CargoNameReplace;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +17,30 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface ICargoNameReplaceService extends IService<CargoNameReplace> {
 
+    /**
+     * 查询货品名称替换list
+     * @param form
+     * @return
+     */
+    List<CargoNameReplace> findCargoNameReplace(CargoNameReplaceForm form);
+
+    /**
+     * 导入货物名称替换表
+     * @param list
+     */
+    void importExcel(List<CargoNameReplace> list);
+
+    /**
+     * 保存`货物名称替换表`
+     * @param form
+     * @return
+     */
+    CommonResult saveCargoNameReplace(CargoNameReplaceForm form);
+
+    /**
+     * 删除`货物名称替换表`
+     * @param id
+     * @return
+     */
+    CommonResult deleteCargoNameReplace(Long id);
 }

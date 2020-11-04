25ba3dfbf8cf7f0ce77b6f40ffee2ef498813bf0
mfcfine <mfcfine@163.com>
2020-11-04 18:16:48 +0800

    南京电商平台 1.报价对应应收费用明细,查询list

--- jayud-mall/jayud-mall-service/src/main/java/com/jayud/mall/service/ITemplateCopeReceivableService.java
+++ jayud-mall/jayud-mall-service/src/main/java/com/jayud/mall/service/ITemplateCopeReceivableService.java
@@ -1,7 +1,10 @@
 package com.jayud.mall.service;
 
+import com.jayud.mall.model.bo.TemplateCopeReceivableForm;
 import com.jayud.mall.model.po.TemplateCopeReceivable;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +16,10 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface ITemplateCopeReceivableService extends IService<TemplateCopeReceivable> {
 
+    /**
+     * 查询报价对应应收费用明细list
+     * @param form
+     * @return
+     */
+    List<TemplateCopeReceivable> findTemplateCopeReceivable(TemplateCopeReceivableForm form);
 }

ac073fbc5005e4b898384e38ced457646ae9af5a
李达荣 <365165904@qq.com>
2020-11-05 09:08:03 +0800

    sql 删除库名

--- jayud-oms/jayud.sql
+++ jayud-oms/jayud.sql
@@ -16,20 +16,20 @@ CREATE TABLE `cost_genre` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='基础数据费用类型';
 
 -- 2020年10月31日李达荣，功能描述：删除供应商表中票后字段
-ALTER TABLE `jayud-test`.`supplier_info`
+ALTER TABLE `supplier_info`
 DROP COLUMN `after_the_vote`;
 
 -- 2020年11月2日李达荣，功能描述：合同信息表增加合同类型和绑定业务id
-ALTER TABLE `jayud-test`.`contract_info`
+ALTER TABLE `contract_info`
 ADD COLUMN `type` char(5) NOT NULL COMMENT '合同类型' AFTER `updated_time`,
 ADD COLUMN `bind_id` bigint(20) NOT NULL COMMENT '合同绑定的业务id' AFTER `type`;
 
 -- 2020年11月2日李达荣，功能描述：系统用户表增加描述
-ALTER TABLE `jayud-test`.`system_user`
+ALTER TABLE `system_user`
 MODIFY COLUMN `user_type` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '1-用户 2-客户 3-供应商' AFTER `audit_status`;
 
 -- 2020年11月4日李达荣，功能描述：车辆管理增加 牌头电话、更新人、寮步密码、企业代码字段
-ALTER TABLE `jayud-test`.`vehicle_info`
+ALTER TABLE `vehicle_info`
 DROP COLUMN `pt_phone`,
 DROP COLUMN `update_user`,
 DROP COLUMN `stepping_code`,
@@ -41,27 +41,27 @@ ADD COLUMN `stepping_code` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_090
 ADD COLUMN `enterprise_code` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '企业代码' AFTER `stepping_code`;
 
 -- 2020年11月4日李达荣，功能描述：司机管理增加 密码（微信使用）、更新人字段
-ALTER TABLE `jayud-test`.`driver_info`
+ALTER TABLE `driver_info`
 CHANGE COLUMN `up_time` `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间' AFTER `create_time`,
 ADD COLUMN `update_user` varchar(50) NULL COMMENT '更新人' AFTER `update_time`,
 ADD COLUMN `password` varchar(50) NULL COMMENT '密码（微信登录使用）' AFTER `update_user`;
 
 -- 2020年11月4日李达荣，功能描述：添加客户地址表
-CREATE TABLE `jayud-test`.`Untitled`  (
+CREATE TABLE `customer_address` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '地址id',
   `type` char(10) NOT NULL COMMENT '地址类型（0 提货地址 1送货地址）',
-  `customer_id` bigint(20) NOT NULL COMMENT '客户地址id',
+  `customer_id` bigint(20) NOT NULL COMMENT '客户id',
   `contacts` varchar(50) NOT NULL COMMENT '联系人',
   `phone` varchar(20) NOT NULL COMMENT '联系电话',
   `province` varchar(20) NOT NULL COMMENT '省',
   `city` varchar(20) NOT NULL COMMENT '市',
   `area` varchar(20) NOT NULL COMMENT '区',
   `address` varchar(100) NOT NULL COMMENT '详细地址',
-  `postcode` varchar(20) NULL COMMENT '邮编',
-  `status` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT '1' COMMENT '状态（0无效 1有效）',
-  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `create_user` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '创建人',
-  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
-  `update_user` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '更新人',
+  `postcode` varchar(20) DEFAULT NULL COMMENT '邮编',
+  `status` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '1' COMMENT '状态（0无效 1有效）',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `create_user` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '创建人',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  `update_user` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '更新人',
   PRIMARY KEY (`id`)
-) COMMENT = '客户地址';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='客户地址';

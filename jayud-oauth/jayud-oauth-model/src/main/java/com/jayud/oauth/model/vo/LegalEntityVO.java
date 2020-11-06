c8ed62fe76197c5fa77fad00045740a7a68adc78
李达荣 <365165904@qq.com>
2020-11-06 15:15:50 +0800

    法人增加字段

--- jayud-oauth/jayud-oauth-model/src/main/java/com/jayud/oauth/model/vo/LegalEntityVO.java
+++ jayud-oauth/jayud-oauth-model/src/main/java/com/jayud/oauth/model/vo/LegalEntityVO.java
@@ -37,6 +37,25 @@ public class LegalEntityVO {
     private String auditComment;
 
 
+    @ApiModelProperty(value = "英文名")
+    private String legalEnName;
 
+    @ApiModelProperty(value = "电话")
+    private String phone;
+
+    @ApiModelProperty(value = "传真")
+    private String fax;
+
+    @ApiModelProperty(value = "地址")
+    private String address;
+
+    @ApiModelProperty(value = "开户银行")
+    private String bank;
+
+    @ApiModelProperty(value = "开户账户")
+    private String accountOpen;
+
+    @ApiModelProperty(value = "纳税识别号")
+    private String taxIdentificationNum;
 
 }

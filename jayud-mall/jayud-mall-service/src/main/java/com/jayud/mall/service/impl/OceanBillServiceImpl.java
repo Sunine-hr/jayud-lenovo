8de1f57c0adab2f2b0f155e92ff701aa1e02e8f9
mfcfine <mfcfine@163.com>
2020-11-06 13:46:32 +0800

    南京电商平台 1.提单分页查询，并设置柜号list

--- jayud-mall/jayud-mall-service/src/main/java/com/jayud/mall/service/impl/OceanBillServiceImpl.java
+++ jayud-mall/jayud-mall-service/src/main/java/com/jayud/mall/service/impl/OceanBillServiceImpl.java
@@ -1,15 +1,20 @@
 package com.jayud.mall.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.jayud.mall.mapper.OceanBillMapper;
+import com.jayud.mall.mapper.OceanCounterMapper;
 import com.jayud.mall.model.bo.QueryOceanBillForm;
 import com.jayud.mall.model.po.OceanBill;
+import com.jayud.mall.model.po.OceanCounter;
 import com.jayud.mall.model.vo.OceanBillVO;
 import com.jayud.mall.service.IOceanBillService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -25,6 +30,9 @@ public class OceanBillServiceImpl extends ServiceImpl<OceanBillMapper, OceanBill
     @Autowired
     OceanBillMapper oceanBillMapper;
 
+    @Autowired
+    OceanCounterMapper oceanCounterMapper;
+
     @Override
     public IPage<OceanBillVO> findOceanBillByPage(QueryOceanBillForm form) {
         //处理时间区间
@@ -37,6 +45,16 @@ public class OceanBillServiceImpl extends ServiceImpl<OceanBillMapper, OceanBill
         //定义排序规则
         //page.addOrder(OrderItem.desc("oc.id"));
         IPage<OceanBillVO> pageInfo = oceanBillMapper.findOceanBillByPage(page, form);
+
+        //设置柜号list
+        List<OceanBillVO> records = pageInfo.getRecords();
+        records.forEach(oceanBillVO -> {
+            Long obId = oceanBillVO.getId();
+            QueryWrapper<OceanCounter> queryWrapper = new QueryWrapper<>();
+            queryWrapper.eq("ob_id", obId);
+            List<OceanCounter> oceanCounters = oceanCounterMapper.selectList(queryWrapper);
+            oceanBillVO.setOceanCounterList(oceanCounters);
+        });
         return pageInfo;
     }
 }

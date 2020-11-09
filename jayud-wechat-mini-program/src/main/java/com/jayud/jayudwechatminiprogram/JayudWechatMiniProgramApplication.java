268acbc58f38feb5ca6b16f48532a7a7cbd81408
李达荣 <365165904@qq.com>
2020-11-09 09:36:51 +0800

    初始化小程序项目

---
+++ jayud-wechat-mini-program/src/main/java/com/jayud/jayudwechatminiprogram/JayudWechatMiniProgramApplication.java
@@ -0,0 +1,13 @@
+package com.jayud.jayudwechatminiprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JayudWechatMiniProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JayudWechatMiniProgramApplication.class, args);
+    }
+
+}
---
+++ jayud-wechat-mini-program/src/test/java/com/jayud/jayudwechatminiprogram/JayudWechatMiniProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayud.jayudwechatminiprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JayudWechatMiniProgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

2c2de19be3ad37216774e19babe87d939c20c2cc
李达荣 <365165904@qq.com>
2020-11-12 15:25:24 +0800

    客户地址维护代码更新

--- jayud-oms/jayud-oms-web/src/main/java/com/jayud/oms/controller/DeliveryAddressController.java
+++ jayud-oms/jayud-oms-web/src/main/java/com/jayud/oms/controller/DeliveryAddressController.java
@@ -9,10 +9,12 @@ import com.jayud.common.utils.ConvertUtil;
 import com.jayud.oms.model.bo.AddCustomerAddressForm;
 import com.jayud.oms.model.bo.QueryCustomerAddressForm;
 import com.jayud.oms.model.enums.StatusEnum;
-import com.jayud.oms.model.po.CustomerAddress;
+import com.jayud.oms.model.po.DeliveryAddress;
+import com.jayud.oms.model.po.RegionCity;
 import com.jayud.oms.model.vo.CustomerAddressVO;
 import com.jayud.oms.model.vo.DriverInfoVO;
-import com.jayud.oms.service.ICustomerAddressService;
+import com.jayud.oms.service.IDeliveryAddressService;
+import com.jayud.oms.service.IRegionCityService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
@@ -20,6 +22,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -27,24 +30,26 @@ import java.util.Map;
 
 /**
  * <p>
- * 客户地址 前端控制器
+ * 提货地址基础数据表 前端控制器
  * </p>
  *
  * @author 李达荣
- * @since 2020-11-04
+ * @since 2020-11-12
  */
 @RestController
 @RequestMapping("/customerAddress")
 @Api(tags = "客户地址维护")
-public class CustomerAddressController {
+public class DeliveryAddressController {
 
     @Autowired
-    private ICustomerAddressService customerAddressService;
+    private IDeliveryAddressService deliveryAddressService;
+    @Autowired
+    private IRegionCityService regionCityService;
 
     @ApiOperation(value = "分页查询客户地址")
     @PostMapping(value = "/findCustomerAddressByPage")
     public CommonResult<CommonPageResult<DriverInfoVO>> findCustomerAddressByPage(@RequestBody QueryCustomerAddressForm form) {
-        IPage<CustomerAddressVO> iPage = customerAddressService.findCustomerAddressByPage(form);
+        IPage<CustomerAddressVO> iPage = deliveryAddressService.findCustomerAddressByPage(form);
         iPage.getRecords().forEach(CustomerAddressVO::splicingAddress);
         return CommonResult.success(new CommonPageResult<>(iPage));
     }
@@ -52,8 +57,24 @@ public class CustomerAddressController {
     @ApiOperation(value = "新增编辑客户地址")
     @PostMapping(value = "/saveOrUpdateCustomerAddress")
     public CommonResult saveOrUpdateCustomerAddress(@Valid @RequestBody AddCustomerAddressForm form) {
-        CustomerAddress customerAddress = ConvertUtil.convert(form, CustomerAddress.class);
-        if (this.customerAddressService.saveOrUpdateCustomerAddress(customerAddress)) {
+        DeliveryAddress deliveryAddress = ConvertUtil.convert(form, DeliveryAddress.class);
+
+        //查询省，市代码,默认国家代码
+        if (form.getId() == null) {
+            {
+                RegionCity province = regionCityService.getById(form.getProvince());
+                RegionCity city = regionCityService.getById(form.getCity());
+                deliveryAddress.setCountryName("中国")
+                        .setCountryCode(56)
+                        .setStateCode(province.getCode())
+                        .setStateName(province.getName())
+                        .setCityCode(city.getCode())
+                        .setCityName(city.getName());
+
+            }
+        }
+
+        if (this.deliveryAddressService.saveOrUpdateCustomerAddress(deliveryAddress)) {
             return CommonResult.success();
         } else {
             return CommonResult.error(ResultEnum.OPR_FAIL);
@@ -66,11 +87,11 @@ public class CustomerAddressController {
         if (StringUtils.isEmpty(map.get("id"))) {
             return CommonResult.error(500, "id is required");
         }
-        Long id = Long.parseLong(map.get("id"));
-        CustomerAddress tmp = this.customerAddressService.getById(id);
-        String status=StatusEnum.ENABLE.getCode().equals(tmp.getStatus())?StatusEnum.INVALID.getCode():StatusEnum.ENABLE.getCode();
-        CustomerAddress customerAddress = new CustomerAddress().setId(id).setStatus(status);
-        if (this.customerAddressService.saveOrUpdateCustomerAddress(customerAddress)) {
+        Integer id = Integer.valueOf(map.get("id"));
+        DeliveryAddress tmp = this.deliveryAddressService.getById(id);
+        String status = StatusEnum.ENABLE.getCode().equals(String.valueOf(tmp.getStatus())) ? StatusEnum.INVALID.getCode() : StatusEnum.ENABLE.getCode();
+        DeliveryAddress deliveryAddress = new DeliveryAddress().setId(id).setStatus(Integer.valueOf(status));
+        if (this.deliveryAddressService.saveOrUpdateCustomerAddress(deliveryAddress)) {
             return CommonResult.success();
         } else {
             return CommonResult.error(ResultEnum.OPR_FAIL);
@@ -84,9 +105,8 @@ public class CustomerAddressController {
             return CommonResult.error(500, "id is required");
         }
         Long id = Long.parseLong(map.get("id"));
-        CustomerAddress customerAddress = this.customerAddressService.getById(id);
-        return CommonResult.success(ConvertUtil.convert(customerAddress, CustomerAddressVO.class));
+        DeliveryAddress deliveryAddress = this.deliveryAddressService.getById(id);
+        return CommonResult.success(ConvertUtil.convert(deliveryAddress, CustomerAddressVO.class));
     }
-
 }
 

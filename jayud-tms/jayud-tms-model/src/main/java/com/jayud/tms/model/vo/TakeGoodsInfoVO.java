6bb9c1767cff5d3ba86d6e149f5be8154040b117
zcm2012888 <2373329941@qq.com>
2020-11-14 11:52:14 +0800

    车型柜号

--- jayud-tms/jayud-tms-model/src/main/java/com/jayud/tms/model/vo/TakeGoodsInfoVO.java
+++ jayud-tms/jayud-tms-model/src/main/java/com/jayud/tms/model/vo/TakeGoodsInfoVO.java
@@ -30,6 +30,15 @@ public class TakeGoodsInfoVO {
     @ApiModelProperty(value = "车型(1-3T 2-5t 3-8T 4-10T)")
     private Integer vehicleSize;
 
+    @ApiModelProperty(value = "车型(1吨车 2柜车)")
+    private String vehicleTypeDesc;
+
+    @ApiModelProperty(value = "柜号")
+    private String cntrNo;
+
+    @ApiModelProperty(value = "车型(1-3T 2-5t 3-8T 4-10T)")
+    private String vehicleSizeDesc;
+
     @ApiModelProperty(value = "提货日期")
     private String takeTimeStr;
 

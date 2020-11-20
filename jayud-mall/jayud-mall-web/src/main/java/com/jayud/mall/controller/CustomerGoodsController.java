54479038bb462569757bacc7cb2c2895816c7666
mfcfine <mfcfine@163.com>
2020-11-20 08:45:48 +0800

    C端-客户商品表接口

---
+++ jayud-mall/jayud-mall-web/src/main/java/com/jayud/mall/controller/CustomerGoodsController.java
@@ -0,0 +1,14 @@
+package com.jayud.mall.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customergoods")
+@Api(tags = "C端-客户商品表接口")
+public class CustomerGoodsController {
+
+
+
+}

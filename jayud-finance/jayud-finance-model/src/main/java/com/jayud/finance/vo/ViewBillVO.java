7d7f3bb75778478e83b266caed3821175cac984c
zcm2012888 <2373329941@qq.com>
2020-11-23 18:39:13 +0800

    出账问题

--- jayud-finance/jayud-finance-model/src/main/java/com/jayud/finance/vo/ViewBillVO.java
+++ jayud-finance/jayud-finance-model/src/main/java/com/jayud/finance/vo/ViewBillVO.java
@@ -9,7 +9,7 @@ import lombok.Data;
 @Data
 public class ViewBillVO {
 
-    @ApiModelProperty(value = "接单法人")
+    @ApiModelProperty(value = "接单法人(第一行,FR,公司名称)")
     private String legalName;
 
     @ApiModelProperty(value = "开始核算期")
@@ -29,4 +29,25 @@ public class ViewBillVO {
 
     @ApiModelProperty(value = "制单时间")
     private String makeTimeStr;
+
+    @ApiModelProperty(value = "制单人")
+    private String auditUser;
+
+    @ApiModelProperty(value = "制单时间")
+    private String auditTimeStr;
+
+    @ApiModelProperty(value = "开户银行")
+    private String accountBank;
+
+    @ApiModelProperty(value = "开户账号")
+    private String accountNo;
+
+    @ApiModelProperty(value = "纳税人识别号")
+    private String taxNo;
+
+    @ApiModelProperty(value = "公司地址")
+    private String address;
+
+    @ApiModelProperty(value = "联系电话")
+    private String phone;
 }

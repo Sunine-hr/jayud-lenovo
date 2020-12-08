dfac1a65b9469d7df23056f53ee4755748da5afe
mfcfine <mfcfine@163.com>
2020-12-08 11:30:11 +0800

    查询国家list

---
+++ jayud-mall/jayud-mall-admin/src/main/java/com/jayud/mall/controller/CountryController.java
@@ -0,0 +1,31 @@
+package com.jayud.mall.controller;
+
+import com.jayud.common.CommonResult;
+import com.jayud.mall.model.bo.CountryForm;
+import com.jayud.mall.model.vo.CountryVO;
+import com.jayud.mall.service.ICountryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/country")
+@Api(tags = "后台-国家基础信息接口")
+public class CountryController {
+
+    @Autowired
+    ICountryService countryService;
+
+    @ApiOperation(value = "查询国家list")
+    @PostMapping("/findCountry")
+    public CommonResult<List<CountryVO>> findCountry(@RequestBody CountryForm form) {
+        List<CountryVO> list = countryService.findCountry(form);
+        return CommonResult.success(list);
+    }
+}

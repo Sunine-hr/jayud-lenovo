33e76a4dd7f0654d57e122d3cbe5cb1b97cd1b45
mfcfine <mfcfine@163.com>
2020-12-24 10:36:08 +0800

    国家基础信息接口

--- jayud-mall/jayud-mall-admin/src/main/java/com/jayud/mall/controller/CountryController.java
+++ jayud-mall/jayud-mall-admin/src/main/java/com/jayud/mall/controller/CountryController.java
@@ -6,6 +6,8 @@ import com.jayud.mall.model.vo.CountryVO;
 import com.jayud.mall.service.ICountryService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiOperationSupport;
+import io.swagger.annotations.ApiSort;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +19,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/country")
 @Api(tags = "S008-后台-国家基础信息接口")
+@ApiSort(value = 8)
 public class CountryController {
 
     @Autowired
@@ -24,6 +27,7 @@ public class CountryController {
 
     @ApiOperation(value = "查询国家list")
     @PostMapping("/findCountry")
+    @ApiOperationSupport(order = 1)
     public CommonResult<List<CountryVO>> findCountry(@RequestBody CountryForm form) {
         List<CountryVO> list = countryService.findCountry(form);
         return CommonResult.success(list);
--- jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/bo/QueryCurrencyInfoForm.java
+++ jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/bo/QueryCurrencyInfoForm.java
@@ -1,21 +1,26 @@
 package com.jayud.mall.model.bo;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class QueryCurrencyInfoForm extends BasePageForm {
 
-    @ApiModelProperty(value = "币种代码")
+    @ApiModelProperty(value = "币种代码", position = 1)
+    @JSONField(ordinal = 1)
     private String currencyCode;
 
-    @ApiModelProperty(value = "币种名称")
+    @ApiModelProperty(value = "币种名称", position = 2)
+    @JSONField(ordinal = 2)
     private String currencyName;
 
-    @ApiModelProperty(value = "国家代码")
+    @ApiModelProperty(value = "国家代码", position = 3)
+    @JSONField(ordinal = 3)
     private String countryCode;
 
-    @ApiModelProperty(value = "状态(0无效 1有效)")
+    @ApiModelProperty(value = "状态(0无效 1有效)", position = 4)
+    @JSONField(ordinal = 4)
     private String status;
 
 }

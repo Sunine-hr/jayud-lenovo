611e73e53c2455454b94da0e683059fee3e355e9
mfcfine <mfcfine@163.com>
2020-12-24 10:36:50 +0800

    币种接口

--- jayud-mall/jayud-mall-admin/src/main/java/com/jayud/mall/controller/CurrencyInfoController.java
+++ jayud-mall/jayud-mall-admin/src/main/java/com/jayud/mall/controller/CurrencyInfoController.java
@@ -6,6 +6,8 @@ import com.jayud.mall.model.vo.CurrencyInfoVO;
 import com.jayud.mall.service.ICurrencyInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiOperationSupport;
+import io.swagger.annotations.ApiSort;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +18,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/currencyinfo")
-@Api(tags = "后台-币种接口")
+@Api(tags = "S009-后台-币种接口")
+@ApiSort(value = 9)
 public class CurrencyInfoController {
 
     @Autowired
@@ -24,6 +27,7 @@ public class CurrencyInfoController {
 
     @ApiOperation(value = "查询币种List")
     @PostMapping("/findCurrencyInfo")
+    @ApiOperationSupport(order = 1)
     public CommonResult<List<CurrencyInfoVO>> findCurrencyInfo(@RequestBody QueryCurrencyInfoForm form) {
         List<CurrencyInfoVO> list = currencyInfoService.findCurrencyInfo(form);
         return CommonResult.success(list);

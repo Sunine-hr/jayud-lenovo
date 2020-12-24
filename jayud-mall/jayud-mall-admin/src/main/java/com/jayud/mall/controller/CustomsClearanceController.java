03f83a883a08735e8bc240e7e81ac276594dbaff
mfcfine <mfcfine@163.com>
2020-12-24 14:22:56 +0800

    清关资料接口

--- jayud-mall/jayud-mall-admin/src/main/java/com/jayud/mall/controller/CustomsClearanceController.java
+++ jayud-mall/jayud-mall-admin/src/main/java/com/jayud/mall/controller/CustomsClearanceController.java
@@ -9,6 +9,8 @@ import com.jayud.mall.model.vo.CustomsClearanceVO;
 import com.jayud.mall.service.ICustomsClearanceService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiOperationSupport;
+import io.swagger.annotations.ApiSort;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/customsclearance")
-@Api(tags = "清关资料接口")
+@Api(tags = "S012-后台-清关资料接口")
+@ApiSort(value = 12)
 public class CustomsClearanceController {
 
     @Autowired
@@ -25,6 +28,7 @@ public class CustomsClearanceController {
 
     @ApiOperation(value = "分页查询清关资料")
     @PostMapping("/findCustomsClearanceByPage")
+    @ApiOperationSupport(order = 1)
     public CommonResult<CommonPageResult<CustomsClearanceVO>> findCustomsClearanceByPage(@RequestBody QueryCustomsClearanceForm form) {
         IPage<CustomsClearanceVO> pageList = customsClearanceService.findCustomsClearanceByPage(form);
         CommonPageResult<CustomsClearanceVO> pageVO = new CommonPageResult(pageList);
@@ -33,6 +37,7 @@ public class CustomsClearanceController {
 
     @ApiOperation(value = "保存清关资料")
     @PostMapping("/saveCustomsClearance")
+    @ApiOperationSupport(order = 2)
     public CommonResult saveCustomsClearance(@RequestBody CustomsClearanceForm form){
         return customsClearanceService.saveCustomsData(form);
     }

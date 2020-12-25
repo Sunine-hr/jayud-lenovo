979961cfa6abd5546d117f64f40cce3d129a759a
zcm2012888 <2373329941@qq.com>
2020-12-25 17:33:49 +0800

    BUG-422

--- jayud-finance/jayud-finance-service/src/main/java/com/jayud/finance/service/impl/OrderReceivableBillDetailServiceImpl.java
+++ jayud-finance/jayud-finance-service/src/main/java/com/jayud/finance/service/impl/OrderReceivableBillDetailServiceImpl.java
@@ -567,6 +567,16 @@ public class OrderReceivableBillDetailServiceImpl extends ServiceImpl<OrderRecei
                 QueryWrapper updateWrapper = new QueryWrapper();
                 updateWrapper.eq("bill_no",billNo);
                 update(orderReceivableBillDetail,updateWrapper);
+
+                //保存审核信息
+                AuditInfoForm auditInfoForm = new AuditInfoForm();
+                auditInfoForm.setExtUniqueFlag(billNo);
+                auditInfoForm.setAuditTypeDesc("应收客服反审核");
+                auditInfoForm.setAuditStatus(BillEnum.B_7.getCode());
+                auditInfoForm.setAuditComment(form.getRemark());
+                auditInfoForm.setExtDesc("order_receivable_bill_detail表bill_no");
+                auditInfoForm.setAuditUser(form.getLoginUserName());
+                omsClient.saveAuditInfo(auditInfoForm);
             }
         }else if("cw_s_reject".equals(form.getCmd())){
             for (OrderReceivableBillDetail existObject : existList) {
@@ -582,6 +592,16 @@ public class OrderReceivableBillDetailServiceImpl extends ServiceImpl<OrderRecei
                 QueryWrapper updateWrapper = new QueryWrapper();
                 updateWrapper.eq("bill_no",billNo);
                 update(orderReceivableBillDetail,updateWrapper);
+
+                //保存审核信息
+                AuditInfoForm auditInfoForm = new AuditInfoForm();
+                auditInfoForm.setExtUniqueFlag(billNo);
+                auditInfoForm.setAuditTypeDesc("应收财务反审核");
+                auditInfoForm.setAuditStatus(BillEnum.B_8.getCode());
+                auditInfoForm.setAuditComment(form.getRemark());
+                auditInfoForm.setExtDesc("order_receivable_bill_detail表bill_no");
+                auditInfoForm.setAuditUser(form.getLoginUserName());
+                omsClient.saveAuditInfo(auditInfoForm);
             }
         }
         return CommonResult.success();

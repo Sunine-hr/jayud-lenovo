dc71a367c2b80485246dd0565cbe95794220b081
李达荣 <365165904@qq.com>
2020-12-30 10:11:22 +0800

    1.vivo派车信息推送改为实时调用 2.查询中港子订单数据sql更改

--- jayud-tms/jayud-tms-service/src/main/java/com/jayud/tms/feign/FreightAirApiClient.java
+++ jayud-tms/jayud-tms-service/src/main/java/com/jayud/tms/feign/FreightAirApiClient.java
@@ -2,8 +2,10 @@ package com.jayud.tms.feign;
 
 
 import com.jayud.common.ApiResult;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -17,5 +19,12 @@ public interface FreightAirApiClient {
      */
     @PostMapping("/airfreight/toVivo/forwarder/forwarderDispatchRejected")
     public ApiResult forwarderDispatchRejected(@RequestParam("dispatchNo") String dispatchNo);
+
+
+    /**
+     * 车辆信息传给vivo
+     */
+    @PostMapping("/airfreight/toVivo/forwarder/vehicleInfo")
+    public ApiResult forwarderVehicleInfo(@RequestBody String value);
 
 }

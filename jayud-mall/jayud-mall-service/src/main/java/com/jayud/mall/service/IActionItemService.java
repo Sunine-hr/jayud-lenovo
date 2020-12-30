8586de43ebee46832e8971a5f4c31703eb3d361c
mfcfine <mfcfine@163.com>
2020-12-30 14:38:43 +0800

    操作项

---
+++ jayud-mall/jayud-mall-service/src/main/java/com/jayud/mall/service/IActionItemService.java
@@ -0,0 +1,16 @@
+package com.jayud.mall.service;
+
+import com.jayud.mall.model.po.ActionItem;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 操作项 服务类
+ * </p>
+ *
+ * @author fachang.mao
+ * @since 2020-12-30
+ */
+public interface IActionItemService extends IService<ActionItem> {
+
+}
---
+++ jayud-mall/jayud-mall-service/src/main/java/com/jayud/mall/service/impl/ActionItemServiceImpl.java
@@ -0,0 +1,20 @@
+package com.jayud.mall.service.impl;
+
+import com.jayud.mall.model.po.ActionItem;
+import com.jayud.mall.mapper.ActionItemMapper;
+import com.jayud.mall.service.IActionItemService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 操作项 服务实现类
+ * </p>
+ *
+ * @author fachang.mao
+ * @since 2020-12-30
+ */
+@Service
+public class ActionItemServiceImpl extends ServiceImpl<ActionItemMapper, ActionItem> implements IActionItemService {
+
+}

a213b16f99744f3aac66becf5c96fc5feffc9d32
mfcfine <mfcfine@163.com>
2020-12-30 14:44:17 +0800

    操作项组合

---
+++ jayud-mall/jayud-mall-service/src/main/java/com/jayud/mall/service/IActionCombinationService.java
@@ -0,0 +1,16 @@
+package com.jayud.mall.service;
+
+import com.jayud.mall.model.po.ActionCombination;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 操作项组合 服务类
+ * </p>
+ *
+ * @author fachang.mao
+ * @since 2020-12-30
+ */
+public interface IActionCombinationService extends IService<ActionCombination> {
+
+}
---
+++ jayud-mall/jayud-mall-service/src/main/java/com/jayud/mall/service/impl/ActionCombinationServiceImpl.java
@@ -0,0 +1,20 @@
+package com.jayud.mall.service.impl;
+
+import com.jayud.mall.model.po.ActionCombination;
+import com.jayud.mall.mapper.ActionCombinationMapper;
+import com.jayud.mall.service.IActionCombinationService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 操作项组合 服务实现类
+ * </p>
+ *
+ * @author fachang.mao
+ * @since 2020-12-30
+ */
+@Service
+public class ActionCombinationServiceImpl extends ServiceImpl<ActionCombinationMapper, ActionCombination> implements IActionCombinationService {
+
+}

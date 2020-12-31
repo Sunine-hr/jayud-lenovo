631544a2ae1f20d4f5652c6e33ef53c29ca25e7f
mfcfine <mfcfine@163.com>
2020-12-31 17:14:40 +0800

    S032-后台-报价类型表接口

--- jayud-mall/jayud-mall-admin/src/main/java/com/jayud/mall/controller/QuotationTypeController.java
+++ jayud-mall/jayud-mall-admin/src/main/java/com/jayud/mall/controller/QuotationTypeController.java
@@ -5,6 +5,8 @@ import com.jayud.mall.model.vo.QuotationTypeReturnVO;
 import com.jayud.mall.service.IQuotationTypeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiOperationSupport;
+import io.swagger.annotations.ApiSort;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/quotationtype")
-@Api(tags = "报价类型表接口")
+@Api(tags = "S032-后台-报价类型表接口")
+@ApiSort(value = 32)
 public class QuotationTypeController {
 
     @Autowired
@@ -20,6 +23,7 @@ public class QuotationTypeController {
 
     @ApiOperation(value = "报价类型下拉选择")
     @PostMapping("/findQuotationTypeBy")
+    @ApiOperationSupport(order = 1)
     public CommonResult<QuotationTypeReturnVO> findQuotationTypeBy(){
         QuotationTypeReturnVO quotationTypeReturnVO = quotationTypeService.findQuotationTypeBy();
         return CommonResult.success(quotationTypeReturnVO);
--- jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/vo/QuotationTypeReturnVO.java
+++ jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/vo/QuotationTypeReturnVO.java
@@ -1,5 +1,6 @@
 package com.jayud.mall.model.vo;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -9,9 +10,11 @@ import java.util.List;
 public class QuotationTypeReturnVO {
 
     @ApiModelProperty(value = "整柜", position = 1)
+    @JSONField(ordinal = 1)
     private List<QuotationTypeVO> fullContainer;
 
     @ApiModelProperty(value = "散柜", position = 2)
+    @JSONField(ordinal = 2)
     private List<QuotationTypeVO> scatteredArk;
 
 }

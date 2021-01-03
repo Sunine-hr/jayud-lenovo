2b45d4e927dbdff848184126254ab15c81ab1599
mfcfine <mfcfine@163.com>
2021-01-03 22:00:44 +0800

    S041-后台-提单对应货柜信息接口

--- jayud-mall/jayud-mall-admin/src/main/java/com/jayud/mall/controller/OceanCounterController.java
+++ jayud-mall/jayud-mall-admin/src/main/java/com/jayud/mall/controller/OceanCounterController.java
@@ -5,6 +5,8 @@ import com.jayud.mall.model.bo.OceanCounterForm;
 import com.jayud.mall.service.IOceanCounterService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiOperationSupport;
+import io.swagger.annotations.ApiSort;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,14 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/oceancounter")
-@Api(tags = "提单对应货柜信息接口")
+@Api(tags = "S041-后台-提单对应货柜信息接口")
+@ApiSort(value = 41)
 public class OceanCounterController {
 
     @Autowired
     IOceanCounterService oceanCounterService;
 
-    @ApiOperation(value = "保存-提单对应货柜信息")
+    @ApiOperation(value = "保存-提单对应货柜信息,没有用了")
     @PostMapping("/saveOceanCounter")
+    @ApiOperationSupport(order = 1)
     public CommonResult saveOceanCounter(@RequestBody OceanCounterForm form){
         return oceanCounterService.saveOceanCounter(form);
     }

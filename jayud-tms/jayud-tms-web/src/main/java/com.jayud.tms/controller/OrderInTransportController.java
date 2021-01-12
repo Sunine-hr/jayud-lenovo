a3609ce4d46e23681f77b34e977d960a72e32866
zcm2012888 <2373329941@qq.com>
2021-01-12 14:41:49 +0800

    BUG

--- jayud-tms/jayud-tms-web/src/main/java/com.jayud.tms/controller/OrderInTransportController.java
+++ jayud-tms/jayud-tms-web/src/main/java/com.jayud.tms/controller/OrderInTransportController.java
@@ -297,7 +297,7 @@ public class OrderInTransportController {
             //当选择的是虚拟仓时系统自动生成入仓出仓数据,即从车辆通关直接到车辆派送
             Boolean isVirtual = false;
             OrderTransport orderTransport1 = orderTransportService.getById(form.getOrderId());
-            if(orderTransport1 != null && orderTransport1.getWarehouseInfoId() == null){
+            if(orderTransport1 != null && orderTransport1.getWarehouseInfoId() != null){
                 isVirtual = omsClient.isVirtualWarehouse(orderTransport1.getWarehouseInfoId()).getData();
             }
             if(isVirtual) {

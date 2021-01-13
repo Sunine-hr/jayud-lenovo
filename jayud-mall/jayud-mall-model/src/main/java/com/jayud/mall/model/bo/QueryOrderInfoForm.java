3b5c0474cddfca47447b4a58328fc99a49258103
mfcfine <mfcfine@163.com>
2021-01-13 17:52:46 +0800

    产品订单表

--- jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/bo/QueryOrderInfoForm.java
+++ jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/bo/QueryOrderInfoForm.java
@@ -1,12 +1,14 @@
 package com.jayud.mall.model.bo;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class QueryOrderInfoForm extends BasePageForm {
 
-    @ApiModelProperty(value = "订单号")
+    @ApiModelProperty(value = "订单号", position = 1)
+    @JSONField(ordinal = 1)
     private String orderNo;
 
 }

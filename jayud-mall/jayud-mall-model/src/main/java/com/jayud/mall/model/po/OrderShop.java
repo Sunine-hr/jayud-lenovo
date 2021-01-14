c7f2c9473204b5d4f761d458dfe32fd64d8ce064
mfcfine <mfcfine@163.com>
2021-01-14 09:07:51 +0800

    订单对应商品

--- jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/po/OrderShop.java
+++ jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/po/OrderShop.java
@@ -1,15 +1,18 @@
 package com.jayud.mall.model.po;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.extension.activerecord.Model;
 import com.baomidou.mybatisplus.annotation.TableId;
-import java.time.LocalDateTime;
-import java.io.Serializable;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -27,22 +30,27 @@ public class OrderShop extends Model<OrderShop> {
 
     private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty(value = "自增id")
+    @ApiModelProperty(value = "自增id", position = 1)
     @TableId(value = "id", type = IdType.AUTO)
+    @JSONField(ordinal = 1)
     private Long id;
 
-    @ApiModelProperty(value = "订单编号(order_info id)")
+    @ApiModelProperty(value = "订单编号(order_info id)", position = 2)
+    @JSONField(ordinal = 2)
     private Integer orderId;
 
-    @ApiModelProperty(value = "商品编号(customer_goods id)")
+    @ApiModelProperty(value = "商品编号(customer_goods id)", position = 3)
+    @JSONField(ordinal = 3)
     private Integer goodId;
 
-    @ApiModelProperty(value = "数量")
+    @ApiModelProperty(value = "数量", position = 4)
+    @JSONField(ordinal = 4)
     private Integer quantity;
 
-    @ApiModelProperty(value = "创建时间")
+    @ApiModelProperty(value = "创建时间", position = 5)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
+    @JSONField(ordinal = 5, format="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;
-
 
     @Override
     protected Serializable pkVal() {

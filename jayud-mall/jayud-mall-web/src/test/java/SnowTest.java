6a815b7c02b7d6877d78dc61386b8e863ea4f5c3
mfcfine <mfcfine@163.com>
2021-01-14 12:25:44 +0800

    Snow

--- jayud-mall/jayud-mall-web/src/test/java/SnowTest.java
+++ jayud-mall/jayud-mall-web/src/test/java/SnowTest.java
@@ -1,3 +1,4 @@
+import com.jayud.common.utils.IdGenerator;
 import com.jayud.mall.utils.SnowflakeUtils;
 import org.junit.Test;
 
@@ -6,24 +7,30 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 
-public class SnowflakeTest {
+public class SnowTest {
 
-    /**
-     * SnowflakeUtils,生产单号
-     */
     @Test
     public void test1() throws FileNotFoundException {
-        File f=new File("D://out.txt");
+        File f=new File("D://out2.txt");
         FileOutputStream fileOutputStream = new FileOutputStream(f);
         PrintStream printStream = new PrintStream(fileOutputStream);
         System.setOut(printStream);
-        System.out.println("默认输出到了文件 out.txt");
+        System.out.println("默认输出到了文件 out2.txt");
         long start = System.currentTimeMillis();
         for (int i=0; i<1000000; i++){
-            long orderNo = SnowflakeUtils.getOrderNo();
-            System.out.println(orderNo);
+            String longNextId = IdGenerator.getLongNextId();
+            System.out.println(longNextId);//19位
         }
         long end = System.currentTimeMillis();
         System.out.println("currentTimeMillis:   " + (end - start));
+
+    }
+
+    @Test
+    public void test2(){
+        for (int i = 0; i<100; i++){
+            String orderNo = String.valueOf(SnowflakeUtils.getOrderNo());//雪花算法生成订单id
+            System.out.println(orderNo);//18位
+        }
     }
 }

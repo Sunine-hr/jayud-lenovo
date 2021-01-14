54342bc8606482de240e7dbf5779532f551bcdf0
mfcfine <mfcfine@163.com>
2021-01-14 10:59:43 +0800

    订单对应报关文件

--- jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/bo/OrderCustomsFileForm.java
+++ jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/bo/OrderCustomsFileForm.java
@@ -1,12 +1,14 @@
 package com.jayud.mall.model.bo;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class OrderCustomsFileForm {
 
-    @ApiModelProperty(value = "自增id")
+    @ApiModelProperty(value = "自增id", position = 1)
+    @JSONField(ordinal = 1)
     private Long id;
 
 }
--- jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/po/OrderCustomsFile.java
+++ jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/po/OrderCustomsFile.java
@@ -1,5 +1,6 @@
 package com.jayud.mall.model.po;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
@@ -28,36 +29,44 @@ public class OrderCustomsFile extends Model<OrderCustomsFile> {
 
     private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty(value = "自增id")
+    @ApiModelProperty(value = "自增id", position = 1)
     @TableId(value = "id", type = IdType.AUTO)
+    @JSONField(ordinal = 1)
     private Long id;
 
-    @ApiModelProperty(value = "订单号(order_info id)")
+    @ApiModelProperty(value = "订单号(order_info id)", position = 2)
+    @JSONField(ordinal = 2)
     private Integer orderId;
 
-    @ApiModelProperty(value = "文件代码")
+    @ApiModelProperty(value = "文件代码(quoted_file id_code)", position = 3)
+    @JSONField(ordinal = 3)
     private String idCode;
 
-    @ApiModelProperty(value = "文件名称")
+    @ApiModelProperty(value = "文件名称(template_file file_name)", position = 4)
+    @JSONField(ordinal = 4)
     private String fileName;
 
-    @ApiModelProperty(value = "是否必要(0否 1是)")
+    @ApiModelProperty(value = "是否必要(0否 1是)", position = 5)
     @TableField(value = "`options`")
+    @JSONField(ordinal = 5)
     private Integer options;
 
-    @ApiModelProperty(value = "是否审核(0否 1是)")
+    @ApiModelProperty(value = "是否审核(0否 1是)", position = 6)
+    @JSONField(ordinal = 6)
     private Integer isCheck;
 
-    @ApiModelProperty(value = "模版文件地址")
+    @ApiModelProperty(value = "模版文件地址", position = 7)
+    @JSONField(ordinal = 7)
     private String templateUrl;
 
-    @ApiModelProperty(value = "说明")
+    @ApiModelProperty(value = "说明", position = 8)
     @TableField(value = "`describe`")
+    @JSONField(ordinal = 8)
     private String describe;
 
-    @ApiModelProperty(value = "审核状态(0审核不通过  1审核通过)")
+    @ApiModelProperty(value = "审核状态(0审核不通过  1审核通过)", position = 9)
+    @JSONField(ordinal = 9)
     private Integer auditStatus;
-
 
     @Override
     protected Serializable pkVal() {

88c980acfe77306c104aecea785c3538b2e0fb57
mfcfine <mfcfine@163.com>
2021-01-15 17:35:37 +0800

    订单关联运价(报价offer_info，报价模板quotation_template)

--- jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/vo/OrderInfoVO.java
+++ jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/vo/OrderInfoVO.java
@@ -166,4 +166,60 @@ public class OrderInfoVO {
     @JSONField(ordinal = 32)
     private List<OrderPickVO> orderPickVOList;
 
+    /*订单关联运价(报价offer_info，报价模板quotation_template)*/
+    @ApiModelProperty(value = "报价名称(商品名称)", position = 33)
+    private String names;
+
+    @ApiModelProperty(value = "类型1整柜 2散柜(货物类型)", position = 34)
+    private Integer types;
+
+    @ApiModelProperty(value = "开船日期", position = 35)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
+    @JSONField(ordinal = 35, format="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime sailTime;
+
+    @ApiModelProperty(value = "截单日期", position = 36)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
+    @JSONField(ordinal = 36, format="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime cutOffTime;
+
+    @ApiModelProperty(value = "截仓日期", position = 37)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
+    @JSONField(ordinal = 37, format="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime jcTime;
+
+    @ApiModelProperty(value = "截亏仓日期", position = 38)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
+    @JSONField(ordinal = 38, format="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime jkcTime;
+
+    @ApiModelProperty(value = "预计到达时间", position = 39)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
+    @JSONField(ordinal = 39, format="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime estimatedTime;
+
+    @ApiModelProperty(value = "航程(天)", position = 40)
+    @JSONField(ordinal = 40)
+    private String voyageDay;
+
+    @ApiModelProperty(value = "操作信息(操作说明)", position = 41)
+    @JSONField(ordinal = 41)
+    private String remarks;
+
+    @ApiModelProperty(value = "起运港(harbour_info id_code)", position = 42)
+    @JSONField(ordinal = 42)
+    private String startShipment;
+
+    @ApiModelProperty(value = "目的港(harbour_info id_code)", position = 43)
+    @JSONField(ordinal = 43)
+    private String destinationPort;
+
+    @ApiModelProperty(value = "起运港", position = 44)
+    @JSONField(ordinal = 44)
+    private String startShipmentName;
+
+    @ApiModelProperty(value = "目的港", position = 45)
+    @JSONField(ordinal = 45)
+    private String destinationPortName;
+
 }

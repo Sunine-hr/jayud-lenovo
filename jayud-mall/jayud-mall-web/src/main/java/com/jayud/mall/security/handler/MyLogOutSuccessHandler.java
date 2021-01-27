dabed8ac00c2141aaf35682ed7bd866ed9621636
mfcfine <mfcfine@163.com>
2021-01-27 10:47:58 +0800

    mall-web

--- jayud-mall/jayud-mall-web/src/main/java/com/jayud/mall/security/handler/MyLogOutSuccessHandler.java
+++ jayud-mall/jayud-mall-web/src/main/java/com/jayud/mall/security/handler/MyLogOutSuccessHandler.java
@@ -1,55 +1,55 @@
-//package com.jayud.mall.security.handler;
+package com.jayud.mall.security.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <p>自定义退出登录行为</p>
+ */
+@Component
+@Slf4j
+public class MyLogOutSuccessHandler implements LogoutSuccessHandler {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+
+//        Object principal = authentication.getPrincipal();
+//        String json = JSONObject.toJSONString(principal);
+//        log.info("退出登录成功："+principal);
+//        System.out.println(json);
+////        String username = (String) principal;
+////        System.out.println("退出成功，用户名：{"+username+"}");
+//        // 重定向到登录页
+////        httpServletResponse.sendRedirect("/login.html");
 //
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-//import org.springframework.stereotype.Component;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import java.io.PrintWriter;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-///**
-// * <p>自定义退出登录行为</p>
-// */
-//@Component
-//@Slf4j
-//public class MyLogOutSuccessHandler implements LogoutSuccessHandler {
-//
-//    @Autowired
-//    private ObjectMapper objectMapper;
-//
-//    @Override
-//    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
-//
-////        Object principal = authentication.getPrincipal();
-////        String json = JSONObject.toJSONString(principal);
-////        log.info("退出登录成功："+principal);
-////        System.out.println(json);
-//////        String username = (String) principal;
-//////        System.out.println("退出成功，用户名：{"+username+"}");
-////        // 重定向到登录页
-//////        httpServletResponse.sendRedirect("/login.html");
-////
-////        httpServletResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
-////        httpServletResponse.setContentType("application/json;charset=utf-8");
-////        httpServletResponse.getWriter().write("退出成功，请重新登录");
-//
-//        Map<String,Object> map = new HashMap<String,Object>();
-//        map.put("code",200);
-//        map.put("message","退出成功");
-//        map.put("data",authentication);
-//        response.setContentType("application/json;charset=utf-8");
-//        response.setStatus(HttpServletResponse.SC_OK);
-//        PrintWriter out = response.getWriter();
-//        out.write(objectMapper.writeValueAsString(map));
-//        out.flush();
-//        out.close();
-//
-//    }
-//}
+//        httpServletResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
+//        httpServletResponse.setContentType("application/json;charset=utf-8");
+//        httpServletResponse.getWriter().write("退出成功，请重新登录");
+
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("code",200);
+        map.put("message","退出成功");
+        map.put("data",authentication);
+        response.setContentType("application/json;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        PrintWriter out = response.getWriter();
+        out.write(objectMapper.writeValueAsString(map));
+        out.flush();
+        out.close();
+
+    }
+}
--- jayud-mall/jayud-mall-web/src/main/java/com/jayud/mall/security/utils/ContextHolderUtils.java
+++ jayud-mall/jayud-mall-web/src/main/java/com/jayud/mall/security/utils/ContextHolderUtils.java
@@ -1,23 +1,23 @@
-//package com.jayud.mall.security.utils;
-//
-//import org.springframework.web.context.request.RequestContextHolder;
-//import org.springframework.web.context.request.ServletRequestAttributes;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpSession;
-//
-///**
-// * 上下文持有人工具类
-// */
-//public class ContextHolderUtils {
-//
-//    public static HttpServletRequest getRequest() {
-//        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-//        return request;
-//    }
-//
-//    public static HttpSession getSession() {
-//        HttpSession session = getRequest().getSession();
-//        return session;
-//    }
-//}
+package com.jayud.mall.security.utils;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * 上下文持有人工具类
+ */
+public class ContextHolderUtils {
+
+    public static HttpServletRequest getRequest() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        return request;
+    }
+
+    public static HttpSession getSession() {
+        HttpSession session = getRequest().getSession();
+        return session;
+    }
+}

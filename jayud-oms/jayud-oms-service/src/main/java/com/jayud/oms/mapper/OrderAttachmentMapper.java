97985c1c4f0a4f29eb6515fd12724aa24e2a1c55
李达荣 <365165904@qq.com>
2021-02-02 18:17:45 +0800

    追加@mapper

--- jayud-oms/jayud-oms-service/src/main/java/com/jayud/oms/mapper/OrderAttachmentMapper.java
+++ jayud-oms/jayud-oms-service/src/main/java/com/jayud/oms/mapper/OrderAttachmentMapper.java
@@ -2,6 +2,7 @@ package com.jayud.oms.mapper;
 
 import com.jayud.oms.model.po.OrderAttachment;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
@@ -11,6 +12,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author LDR
  * @since 2021-02-02
  */
+@Mapper
 public interface OrderAttachmentMapper extends BaseMapper<OrderAttachment> {
 
 }

4bdd59c80eb275e621175b1250d6d6d7f44b6653
mfcfine <mfcfine@163.com>
2021-02-22 16:53:44 +0800

    admin，订单管理-查看配载信息

--- jayud-mall/jayud-mall-admin/src/main/java/com/jayud/mall/controller/OrderInfoController.java
+++ jayud-mall/jayud-mall-admin/src/main/java/com/jayud/mall/controller/OrderInfoController.java
@@ -96,7 +96,7 @@ public class OrderInfoController {
     }
 
     //订单管理-查看配载信息
-    @ApiOperation(value = "订单管理-查看配载信息(待完成)")
+    @ApiOperation(value = "订单管理-查看配载信息")
     @PostMapping("/lookOrderInfoConf")
     @ApiOperationSupport(order = 9)
     public CommonResult<OrderInfoVO> lookOrderInfoConf(@RequestBody OrderInfoForm form){

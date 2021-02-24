fdbbbd2afe60bf6c616dc8eeb4d15c7d0551e734
mfcfine <mfcfine@163.com>
2021-02-24 18:55:05 +0800

    admin，client，订单对应应付费用明细

--- jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/bo/OrderCopeWithForm.java
+++ jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/bo/OrderCopeWithForm.java
@@ -1,5 +1,6 @@
 package com.jayud.mall.model.bo;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -8,25 +9,36 @@ import java.math.BigDecimal;
 @Data
 public class OrderCopeWithForm {
 
-    @ApiModelProperty(value = "自增id")
+    @ApiModelProperty(value = "自增id", position = 1)
+    @JSONField(ordinal = 1)
     private Long id;
 
-    @ApiModelProperty(value = "订单ID(order_info id)")
+    @ApiModelProperty(value = "订单ID(order_info id)", position = 2)
+    @JSONField(ordinal = 2)
     private Long orderId;
 
-    @ApiModelProperty(value = "费用名称")
+    @ApiModelProperty(value = "费用代码(cost_item cost_code)", position = 3)
+    @JSONField(ordinal = 3)
+    private String costCode;
+
+    @ApiModelProperty(value = "费用名称(cost_item cost_name)", position = 4)
+    @JSONField(ordinal = 4)
     private String costName;
 
-    @ApiModelProperty(value = "供应商代码(supplier_info supplier_code)")
-    private String supplierCode;
+    @ApiModelProperty(value = "供应商id(supplier_info id)", position = 5)
+    @JSONField(ordinal = 5)
+    private Integer supplierId;
 
-    @ApiModelProperty(value = "金额")
+    @ApiModelProperty(value = "金额", position = 6)
+    @JSONField(ordinal = 6)
     private BigDecimal amount;
 
-    @ApiModelProperty(value = "币种(currency_info id)")
+    @ApiModelProperty(value = "币种(currency_info id)", position = 7)
+    @JSONField(ordinal = 7)
     private Integer cid;
 
-    @ApiModelProperty(value = "描述")
+    @ApiModelProperty(value = "描述", position = 8)
+    @JSONField(ordinal = 8)
     private String remarks;
 
 
--- jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/vo/OrderCopeWithVO.java
+++ jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/vo/OrderCopeWithVO.java
@@ -1,5 +1,6 @@
 package com.jayud.mall.model.vo;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -8,36 +9,50 @@ import java.math.BigDecimal;
 @Data
 public class OrderCopeWithVO {
 
-    @ApiModelProperty(value = "自增id")
+    @ApiModelProperty(value = "自增id", position = 1)
+    @JSONField(ordinal = 1)
     private Long id;
 
-    @ApiModelProperty(value = "订单ID(order_info id)")
+    @ApiModelProperty(value = "订单ID(order_info id)", position = 2)
+    @JSONField(ordinal = 2)
     private Long orderId;
 
-    @ApiModelProperty(value = "费用名称")
+    @ApiModelProperty(value = "费用代码(cost_item cost_code)", position = 3)
+    @JSONField(ordinal = 3)
+    private String costCode;
+
+    @ApiModelProperty(value = "费用名称(cost_item cost_name)", position = 4)
+    @JSONField(ordinal = 4)
     private String costName;
 
-    @ApiModelProperty(value = "供应商代码(supplier_info supplier_code)")
-    private String supplierCode;
+    @ApiModelProperty(value = "供应商id(supplier_info id)", position = 5)
+    @JSONField(ordinal = 5)
+    private Integer supplierId;
 
-    @ApiModelProperty(value = "金额")
+    @ApiModelProperty(value = "金额", position = 6)
+    @JSONField(ordinal = 6)
     private BigDecimal amount;
 
-    @ApiModelProperty(value = "币种(currency_info id)")
+    @ApiModelProperty(value = "币种(currency_info id)", position = 7)
+    @JSONField(ordinal = 7)
     private Integer cid;
 
-    @ApiModelProperty(value = "描述")
+    @ApiModelProperty(value = "描述", position = 8)
+    @JSONField(ordinal = 8)
     private String remarks;
 
     /*供应商信息:supplier_info*/
-    @ApiModelProperty(value = "供应商名称(中)")
-    private String supplierChName;
+    @ApiModelProperty(value = "供应商名称(公司名称 supplier_info.company_name)", position = 9)
+    @JSONField(ordinal = 9)
+    private String supplierName;
 
     /*币种:currency_info*/
-    @ApiModelProperty(value = "币种代码")
+    @ApiModelProperty(value = "币种代码", position = 10)
+    @JSONField(ordinal = 10)
     private String currencyCode;
 
-    @ApiModelProperty(value = "币种名称")
+    @ApiModelProperty(value = "币种名称", position = 11)
+    @JSONField(ordinal = 11)
     private String currencyName;
 
 

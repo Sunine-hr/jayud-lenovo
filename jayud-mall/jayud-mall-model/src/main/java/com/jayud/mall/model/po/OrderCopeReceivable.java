5b5311df65f0d9922155cb67261ceeb2ff1ba92c
mfcfine <mfcfine@163.com>
2021-02-24 18:29:58 +0800

    admin，client，订单对应应收费用明细

--- jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/po/OrderCopeReceivable.java
+++ jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/po/OrderCopeReceivable.java
@@ -1,5 +1,6 @@
 package com.jayud.mall.model.po;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
@@ -28,25 +29,34 @@ public class OrderCopeReceivable extends Model<OrderCopeReceivable> {
 
     private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty(value = "自增id")
+    @ApiModelProperty(value = "自增id", position = 1)
     @TableId(value = "id", type = IdType.AUTO)
+    @JSONField(ordinal = 1)
     private Long id;
 
-    @ApiModelProperty(value = "订单ID(order_info id)")
+    @ApiModelProperty(value = "订单ID(order_info id)", position = 2)
+    @JSONField(ordinal = 2)
     private Long orderId;
 
-    @ApiModelProperty(value = "费用名称")
+    @ApiModelProperty(value = "费用代码(cost_item cost_code)", position = 3)
+    @JSONField(ordinal = 3)
+    private String costCode;
+
+    @ApiModelProperty(value = "费用名称(cost_item cost_name)", position = 4)
+    @JSONField(ordinal = 4)
     private String costName;
 
-    @ApiModelProperty(value = "金额")
+    @ApiModelProperty(value = "金额", position = 5)
+    @JSONField(ordinal = 5)
     private BigDecimal amount;
 
-    @ApiModelProperty(value = "币种(currency_info id)")
+    @ApiModelProperty(value = "币种(currency_info id)", position = 6)
+    @JSONField(ordinal = 6)
     private Integer cid;
 
-    @ApiModelProperty(value = "描述")
+    @ApiModelProperty(value = "描述", position = 7)
+    @JSONField(ordinal = 7)
     private String remarks;
-
 
     @Override
     protected Serializable pkVal() {

5822e5a03102e4a0a68b0f39642c8912d326b396
liaolingjie <lljArcher520@163.com>
2021-02-26 10:35:13 +0800

    订单号规则化

---
+++ jayud-common/src/main/java/com/jayud/common/enums/OrderTypeEnum.java
@@ -0,0 +1,48 @@
+package com.jayud.common.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Objects;
+
+@Getter
+@AllArgsConstructor
+public enum OrderTypeEnum {
+
+    //订单类型
+    JYD("JYD", "主单号"),
+    TI("TI", "运输进口"),
+    TE("TE", "运输出口"),
+    TL("TL", "内陆运输"),
+    TT("TT", "拖车运输"),
+    AI("AI", "空运进口"),
+    AE("AE", "空运出口"),
+    SI("SI", "海运进口"),
+    SE("SE", "海运出口"),
+    WH("WH", "仓储订单"),
+    EC("EC", "电商订单"),
+    BG("BG", "报关订单"),
+    P("P", "派车单号"),
+    ZD("ZD", "对账单号");
+
+    private String code;
+    private String desc;
+
+    public static String getDesc(String code) {
+        for (OrderTypeEnum value : values()) {
+            if (Objects.equals(code, value.getCode())) {
+                return value.getDesc();
+            }
+        }
+        return "";
+    }
+
+    public static String getCode(String desc) {
+        for (OrderTypeEnum value : values()) {
+            if (Objects.equals(desc, value.getDesc())) {
+                return value.getCode();
+            }
+        }
+        return "";
+    }
+}

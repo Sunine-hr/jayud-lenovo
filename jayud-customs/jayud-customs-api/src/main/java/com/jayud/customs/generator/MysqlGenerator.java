7a847982303e8305a3b7a8892593247e18ec0f12
mfcfine <mfcfine@163.com>
2021-03-03 18:19:28 +0800

    admin,A048-admin-基础任务接口

--- jayud-customs/jayud-customs-api/src/main/java/com/jayud/customs/generator/MysqlGenerator.java
+++ jayud-customs/jayud-customs-api/src/main/java/com/jayud/customs/generator/MysqlGenerator.java
@@ -31,7 +31,7 @@ public class MysqlGenerator {
                 "jayud-mall\\jayud-mall-service"
         };
         for (String model : models) {
-            shell(model,"pay_bill_detail");
+            shell(model,"task");
 
 
         }
---
+++ jayud-mall/jayud-mall-service/src/main/java/com/jayud/mall/mapper/TaskMapper.java
@@ -0,0 +1,39 @@
+package com.jayud.mall.mapper;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.jayud.mall.model.bo.QueryTaskForm;
+import com.jayud.mall.model.po.Task;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jayud.mall.model.vo.TaskVO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>
+ * 基础任务表 Mapper 接口
+ * </p>
+ *
+ * @author fachang.mao
+ * @since 2021-03-03
+ */
+@Mapper
+@Component
+public interface TaskMapper extends BaseMapper<Task> {
+
+    /**
+     * 基础任务分页查询
+     * @param page
+     * @param form
+     * @return
+     */
+    IPage<TaskVO> findTaskByPage(Page<TaskVO> page, @Param("form") QueryTaskForm form);
+
+    /**
+     * 根据id，查询任务
+     * @param id
+     * @return
+     */
+    TaskVO findTaskById(@Param("id") Long id);
+}

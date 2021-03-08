db55632ebede79810b5f75bb71cc262407501eb5
李达荣 <365165904@qq.com>
2021-03-08 11:08:16 +0800

    车辆管理修改描述

--- SQL/2020-3-1内陆脚本/车辆管理.sql
+++ SQL/2020-3-1内陆脚本/车辆管理.sql
@@ -2,3 +2,6 @@
 
 ALTER TABLE `vehicle_info`
 ADD COLUMN `type` int(10) NULL COMMENT '车辆类型(0:中港车,1:内陆车)' AFTER `main_driver_id`;
+
+ALTER TABLE `vehicle_info`
+MODIFY COLUMN `vehicle_tonnage` varchar(50) DEFAULT NULL COMMENT '车型尺寸-3T\\5T\\8T\\10T\\12T' AFTER `enterprise_code`;

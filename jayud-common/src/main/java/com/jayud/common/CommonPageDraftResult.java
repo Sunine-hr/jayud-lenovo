a839a828c220ca35cf42458d4cf76ab817703821
mfcfine <mfcfine@163.com>
2021-03-11 11:28:34 +0800

    admin,client,web端分页查询订单列表(统计草稿)

--- jayud-common/src/main/java/com/jayud/common/CommonPageDraftResult.java
+++ jayud-common/src/main/java/com/jayud/common/CommonPageDraftResult.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 public class CommonPageDraftResult<T> extends CommonPageResult<T>{
 
     //分页时，显示分页信息和统计的草稿数量
-    @ApiModelProperty(value = "草稿数量", position = 1)
+    @ApiModelProperty(value = "草稿数量", position = 7)
     private Long draftNum;
 
     public CommonPageDraftResult(IPage pageInfo, Long draftNum) {

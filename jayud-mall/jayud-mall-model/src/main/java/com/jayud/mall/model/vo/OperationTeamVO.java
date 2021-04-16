060bb9289d929b5ab2285cd611471eb59289d0f5
mfcfine <mfcfine@163.com>
2021-04-16 15:23:47 +0800

    swagger配置

--- jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/vo/OperationTeamVO.java
+++ jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/vo/OperationTeamVO.java
@@ -1,6 +1,5 @@
 package com.jayud.mall.model.vo;
 
-import com.alibaba.fastjson.annotation.JSONField;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -11,42 +10,33 @@ import java.util.List;
 @Data
 public class OperationTeamVO {
 
-    @ApiModelProperty(value = "主键id", position = 1)
-    @JSONField(ordinal = 1)
+    @ApiModelProperty(value = "主键id")
     private Long id;
 
-    @ApiModelProperty(value = "小组代码", position = 2)
-    @JSONField(ordinal = 2)
+    @ApiModelProperty(value = "小组代码")
     private String groupCode;
 
-    @ApiModelProperty(value = "小组名称", position = 3)
-    @JSONField(ordinal = 3)
+    @ApiModelProperty(value = "小组名称")
     private String groupName;
 
-    @ApiModelProperty(value = "状态(0无效 1有效)", position = 4)
-    @JSONField(ordinal = 4)
+    @ApiModelProperty(value = "状态(0无效 1有效)")
     private String status;
 
-    @ApiModelProperty(value = "创建人(system_user id)", position = 5)
-    @JSONField(ordinal = 5)
+    @ApiModelProperty(value = "创建人(system_user id)")
     private Long creator;
 
-    @ApiModelProperty(value = "创建时间", position = 6)
+    @ApiModelProperty(value = "创建时间")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
-    @JSONField(ordinal = 6, format="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime creationTime;
 
-    @ApiModelProperty(value = "创建人名称", position = 7)
-    @JSONField(ordinal = 7)
+    @ApiModelProperty(value = "创建人名称")
     private String creatorName;
 
     //人员
-    @ApiModelProperty(value = "运营小组人员（前端：人员）", position = 8)
-    @JSONField(ordinal = 8)
+    @ApiModelProperty(value = "运营小组人员（前端：人员）")
     private String memberUserNames;
 
-    @ApiModelProperty(value = "运营小组人员list", position = 9)
-    @JSONField(ordinal = 9)
+    @ApiModelProperty(value = "运营小组人员list")
     private List<OperationTeamMemberVO> operationTeamMemberVOS;
 
 }

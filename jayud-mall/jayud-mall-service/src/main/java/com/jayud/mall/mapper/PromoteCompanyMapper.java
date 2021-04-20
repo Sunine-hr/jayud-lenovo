000e77408c8e5a680c6a5696484b573c40e5fc93
mfcfine <mfcfine@163.com>
2021-04-20 15:17:22 +0800

    推广公司

--- jayud-mall/jayud-mall-service/src/main/java/com/jayud/mall/mapper/PromoteCompanyMapper.java
+++ jayud-mall/jayud-mall-service/src/main/java/com/jayud/mall/mapper/PromoteCompanyMapper.java
@@ -1,8 +1,15 @@
 package com.jayud.mall.mapper;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.jayud.mall.model.bo.QueryPromoteCompanyForm;
 import com.jayud.mall.model.po.PromoteCompany;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jayud.mall.model.vo.PromoteCompanyVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * <p>
@@ -15,4 +22,7 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface PromoteCompanyMapper extends BaseMapper<PromoteCompany> {
 
+    IPage<PromoteCompanyVO> findPromoteCompanyByPage(Page<PromoteCompanyVO> page, @Param("form") QueryPromoteCompanyForm form);
+
+    List<PromoteCompanyVO> findPromoteCompanyByParentId(@Param("parentId") Integer parentId);
 }
--- jayud-mall/jayud-mall-service/src/main/java/com/jayud/mall/service/impl/PromoteCompanyServiceImpl.java
+++ jayud-mall/jayud-mall-service/src/main/java/com/jayud/mall/service/impl/PromoteCompanyServiceImpl.java
@@ -1,10 +1,19 @@
 package com.jayud.mall.service.impl;
 
-import com.jayud.mall.model.po.PromoteCompany;
-import com.jayud.mall.mapper.PromoteCompanyMapper;
-import com.jayud.mall.service.IPromoteCompanyService;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.metadata.OrderItem;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.jayud.mall.mapper.PromoteCompanyMapper;
+import com.jayud.mall.model.bo.QueryPromoteCompanyForm;
+import com.jayud.mall.model.bo.SavePromoteCompanyForm;
+import com.jayud.mall.model.po.PromoteCompany;
+import com.jayud.mall.model.vo.PromoteCompanyVO;
+import com.jayud.mall.service.IPromoteCompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +26,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class PromoteCompanyServiceImpl extends ServiceImpl<PromoteCompanyMapper, PromoteCompany> implements IPromoteCompanyService {
 
+    @Autowired
+    PromoteCompanyMapper promoteCompanyMapper;
+
+    @Override
+    public IPage<PromoteCompanyVO> findPromoteCompanyByPage(QueryPromoteCompanyForm form) {
+        //定义分页参数
+        Page<PromoteCompanyVO> page = new Page(form.getPageNum(),form.getPageSize());
+        //定义排序规则
+        page.addOrder(OrderItem.asc("t.company_id"));
+        IPage<PromoteCompanyVO> pageInfo = promoteCompanyMapper.findPromoteCompanyByPage(page, form);
+        return pageInfo;
+    }
+
+    @Override
+    public void savePromoteCompany(SavePromoteCompanyForm form) {
+
+    }
+
+    @Override
+    public List<PromoteCompanyVO> findPromoteCompanyByParentId(Integer parentId) {
+        return promoteCompanyMapper.findPromoteCompanyByParentId(parentId);
+    }
 }

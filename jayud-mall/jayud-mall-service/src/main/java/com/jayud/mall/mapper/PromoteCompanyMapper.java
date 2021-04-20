b6a75b5eb2aeac90e5c8ac7481562790121a6feb
mfcfine <mfcfine@163.com>
2021-04-20 14:18:00 +0800

    推广公司表

---
+++ jayud-mall/jayud-mall-service/src/main/java/com/jayud/mall/mapper/PromoteCompanyMapper.java
@@ -0,0 +1,18 @@
+package com.jayud.mall.mapper;
+
+import com.jayud.mall.model.po.PromoteCompany;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ * 推广公司表 Mapper 接口
+ * </p>
+ *
+ * @author fachang.mao
+ * @since 2021-04-20
+ */
+@Mapper
+public interface PromoteCompanyMapper extends BaseMapper<PromoteCompany> {
+
+}

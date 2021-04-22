91ac66ca7511ba0de8f36836a4b253c89393ad51
mfcfine <mfcfine@163.com>
2021-04-22 11:21:18 +0800

    推广设置

--- jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/bo/QueryPromoteOrderForm.java
+++ jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/bo/QueryPromoteOrderForm.java
@@ -12,6 +12,9 @@ public class QueryPromoteOrderForm extends BasePageForm{
     @ApiModelProperty(value = "公司名称/渠道公司名称(promote_company company_name)")
     private String companyName;
 
+    @ApiModelProperty(value = "(客户)公司名")
+    private String clientCompanyName;
+
     @ApiModelProperty(value = "(客户)联系人")
     private String clientContacts;
 

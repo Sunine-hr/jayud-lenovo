360ca158b8a1a3d6f59404a2dca3d4a0cc059d53
mfcfine <mfcfine@163.com>
2021-04-27 21:57:35 +0800

    (提单)报关信息表

--- jayud-mall/jayud-mall-service/src/main/java/com/jayud/mall/mapper/BillCustomsInfoMapper.java
+++ jayud-mall/jayud-mall-service/src/main/java/com/jayud/mall/mapper/BillCustomsInfoMapper.java
@@ -1,54 +1,16 @@
 package com.jayud.mall.mapper;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.jayud.mall.model.bo.BillClearanceInfoForm;
-import com.jayud.mall.model.bo.BillCustomsInfoForm;
-import com.jayud.mall.model.po.BillClearanceInfo;
 import com.jayud.mall.model.po.BillCustomsInfo;
-import com.jayud.mall.model.vo.BillClearanceInfoVO;
-import com.jayud.mall.model.vo.BillCustomsInfoVO;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Component;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 /**
  * <p>
- * 提单对应报关
+ * (提单)报关信息表 Mapper 接口
  * </p>
  *
- * @author Larry
- * @since 2020-12-30
+ * @author fachang.mao
+ * @since 2021-04-27
  */
-@Mapper
-@Component
-public interface BillCustomsInfoMapper extends BaseMapper<BillClearanceInfo> {
-    /**
-     * 集合数据
-     * @param
-     * @return
-     */
-    IPage<BillCustomsInfoVO> findBillCustomsInfoByPage(@Param("form") BillCustomsInfoForm form);
-
-
-    /**
-     * 增加
-     * @param billCustomsInfo
-     */
-    void insertBillCustomsInfo(@Param("billCustomsInfo") BillCustomsInfoForm billCustomsInfo);
-
-    /**
-     * 修改
-     * @param billCustomsInfo
-     */
-    void updateBillCustomsInfo(@Param("billCustomsInfo") BillCustomsInfoForm billCustomsInfo);
-
-    /**
-     * 删除
-     * @param id
-     */
-    void deleteBillCustomsInfo(@Param("id") Long id);
-
+public interface BillCustomsInfoMapper extends BaseMapper<BillCustomsInfo> {
 
 }
--- jayud-mall/jayud-mall-service/src/main/java/com/jayud/mall/service/IBillCustomsInfoService.java
+++ jayud-mall/jayud-mall-service/src/main/java/com/jayud/mall/service/IBillCustomsInfoService.java
@@ -1,44 +1,16 @@
 package com.jayud.mall.service;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.jayud.mall.model.bo.BillCustomsInfoForm;
 import com.jayud.mall.model.po.BillCustomsInfo;
-import com.jayud.mall.model.vo.BillCustomsInfoVO;
-import org.apache.ibatis.annotations.Param;
+import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
  * <p>
- * (提单)清关信息表 服务类
+ * (提单)报关信息表 服务类
  * </p>
  *
  * @author fachang.mao
  * @since 2021-04-27
  */
 public interface IBillCustomsInfoService extends IService<BillCustomsInfo> {
-    /**
-     * 集合数据
-     * @param
-     * @return
-     */
-    IPage<BillCustomsInfoVO> findBillCustomsInfoByPage(@Param("form") BillCustomsInfoForm form);
 
-
-    /**
-     * 增加
-     * @param billCustomsInfo
-     */
-    void insertBillCustomsInfo(@Param("billCustomsInfo") BillCustomsInfoForm billCustomsInfo);
-
-    /**
-     * 修改
-     * @param billCustomsInfo
-     */
-    void updateBillCustomsInfo(@Param("billCustomsInfo") BillCustomsInfoForm billCustomsInfo);
-
-    /**
-     * 删除
-     * @param id
-     */
-    void deleteBillCustomsInfo(@Param("id") Long id);
 }

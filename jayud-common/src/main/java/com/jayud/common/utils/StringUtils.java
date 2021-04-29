dbeefae2ec788477025ab70f8d9f11b194804d5a
mfcfine <mfcfine@163.com>
2021-04-29 10:48:19 +0800

    String类型，转换为 utf-8

--- jayud-common/src/main/java/com/jayud/common/utils/StringUtils.java
+++ jayud-common/src/main/java/com/jayud/common/utils/StringUtils.java
@@ -424,4 +424,40 @@ public class StringUtils {
         DecimalFormat df=new DecimalFormat("0000");
         return df.format(count);
     }
+
+
+    /**
+     * String类型，转换为 utf-8
+     * 下载或者导出excel，设置中文名称有用到
+     * @param s
+     * @return
+     */
+    public static String toUtf8String(String s) {
+        StringBuffer sb = new StringBuffer();
+        for (int i=0;i<s.length();i++) {
+            char c = s.charAt(i);
+            if (c >= 0 && c <= 255) {
+                sb.append(c);
+            } else {
+                byte[] b;
+                try {
+                    b = Character.toString(c).getBytes("utf-8");
+                } catch (Exception ex) {
+                    System.out.println(ex);
+                    b = new byte[0];
+                }
+                for (int j = 0; j < b.length; j++) {
+                    int k = b[j];
+                    if (k < 0) {
+                        k += 256;
+                    }
+                    sb.append("%" + Integer.toHexString(k).
+                            toUpperCase());
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+
 }

1bb0c250d14be01f98554f0f332aa48fec79dbcc
dengxiangqian <xiangqianone@163.com>
2021-05-27 17:06:38 +0800

    报关sql修改

--- SQL/2021-5-28/报关.sql
+++ SQL/2021-5-28/报关.sql
@@ -1,4 +1,4 @@
 
 --  报关增加备注
 ALTER TABLE `order_customs`
-CHANGE COLUMN `remarks` `order_remarks` varchar(255)  DEFAULT NULL COMMENT '订单备注' AFTER `supervision_mode`;
+ADD COLUMN `order_remarks` varchar(255)  DEFAULT NULL COMMENT '订单备注' AFTER `is_send_mail`;

858e0e608c1b43a4a754890984bf00f99a16080c
mfcfine <mfcfine@163.com>
2021-06-04 18:18:56 +0800

    在柜子清单添加箱子时，自动进行 1.提单 关联 订单 的操作 2.提单 删除 订单 的操作

--- jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/vo/OfferInfoDateVO.java
+++ jayud-mall/jayud-mall-model/src/main/java/com/jayud/mall/model/vo/OfferInfoDateVO.java
@@ -17,23 +17,23 @@ public class OfferInfoDateVO {
     private Integer qie;
 
     @ApiModelProperty(value = "报价-开船日期")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime sailTime;
 
     @ApiModelProperty(value = "报价-截单日期")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime cutOffTime;
 
     @ApiModelProperty(value = "报价-截仓日期")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime jcTime;
 
     @ApiModelProperty(value = "报价-截亏仓日期")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime jkcTime;
 
     @ApiModelProperty(value = "预计到达时间")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime estimatedTime;
 
 }

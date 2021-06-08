3e1a709701dc7ce4dc76ef19d1560a818c13115e
李达荣 <365165904@qq.com>
2021-06-08 11:25:52 +0800

    财务汇率管,间接汇率

--- jayud-finance/jayud-finance-service/src/main/java/com/jayud/finance/service/impl/CurrencyRateServiceImpl.java
+++ jayud-finance/jayud-finance-service/src/main/java/com/jayud/finance/service/impl/CurrencyRateServiceImpl.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.core.metadata.OrderItem;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.jayud.common.CommonResult;
+import com.jayud.common.UserOperator;
 import com.jayud.common.enums.StatusEnum;
 import com.jayud.finance.bo.AddCurrencyManageForm;
 import com.jayud.finance.bo.AddCurrencyRateForm;
@@ -20,8 +21,11 @@ import com.jayud.finance.vo.InitComboxStrVO;
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -64,18 +68,33 @@ public class CurrencyRateServiceImpl extends ServiceImpl<CurrencyRateMapper, Cur
         if (repeatNum != 0) {
             return CommonResult.error(10000, "不允许添加相同币种的汇率");
         }
+
+        //查询币种
+        Map<Long, InitComboxStrVO> currencyMap = this.omsClient.initCurrencyInfo().getData().stream().collect(Collectors.toMap(InitComboxStrVO::getId, e -> e));
         //同一个币种同一个月份只允许配置一条
-        QueryWrapper queryWrapper = new QueryWrapper();
+        QueryWrapper<CurrencyRate> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("month", form.getMonth());
+
+        //查询这个月份汇率管理
+        Map<String, CurrencyRate> map = this.baseMapper.selectList(queryWrapper).stream().collect(Collectors.toMap(e -> e.getOcid() + "-" + e.getDcid(), e -> e));
+
+        //间接汇率
+        List<CurrencyRate> indirectExchangeRates = new ArrayList<>();
         //新增的时候批量操作
         List<CurrencyRate> currencyRates = new ArrayList<>();
         for (AddCurrencyRateForm addCurrencyRateForm : form.getRateFormList()) {
-            queryWrapper.eq("ocid", addCurrencyRateForm.getOcid());
-            queryWrapper.eq("dcid", addCurrencyRateForm.getDcid());
-            List<CurrencyRate> existList = baseMapper.selectList(queryWrapper);
-            if (existList != null && existList.size() > 0) {
+//            queryWrapper.eq("ocid", addCurrencyRateForm.getOcid());
+//            queryWrapper.eq("dcid", addCurrencyRateForm.getDcid());
+
+//            List<CurrencyRate> existList = baseMapper.selectList(queryWrapper);
+//            if (existList != null && existList.size() > 0) {
+//                return CommonResult.error(10000, "该月份已存在相同币种的汇率");
+//            }
+            CurrencyRate tmp = map.get(addCurrencyRateForm.getOcid() + "-" + addCurrencyRateForm.getDcid());
+            if (tmp != null) {
                 return CommonResult.error(10000, "该月份已存在相同币种的汇率");
             }
+
             CurrencyRate currencyRate = new CurrencyRate();
             currencyRate.setOcid(addCurrencyRateForm.getOcid());
             currencyRate.setDcid(addCurrencyRateForm.getDcid());
@@ -83,16 +102,59 @@ public class CurrencyRateServiceImpl extends ServiceImpl<CurrencyRateMapper, Cur
             currencyRate.setMonth(form.getMonth());
             currencyRate.setCreatedUser(form.getLoginUserName());
             currencyRates.add(currencyRate);
+
+            //原币兑人民币才有间接汇率:算法1/原币兑人民币汇率
+            if ("CNY".equals(currencyMap.get(addCurrencyRateForm.getDcid()).getCode())) {
+                CurrencyRate tmpRate = map.get(addCurrencyRateForm.getDcid() + "-" + addCurrencyRateForm.getOcid());
+                BigDecimal indirectExchangeRate = new BigDecimal(1).divide(addCurrencyRateForm.getExchangeRate(), 4, RoundingMode.HALF_EVEN);
+                CurrencyRate indirectRate = new CurrencyRate().setMonth(currencyRate.getMonth()).setExchangeRate(indirectExchangeRate)
+                        .setCreatedTime(currencyRate.getCreatedTime()).setCreatedUser(currencyRate.getCreatedUser())
+                        .setOcid(currencyRate.getDcid()).setDcid(currencyRate.getOcid());
+                if (tmpRate != null) {
+                    indirectRate.setId(tmpRate.getId());
+                }
+                indirectExchangeRates.add(indirectRate);
+            }
+
         }
+        //批量处理间接汇率
+        saveOrUpdateBatch(indirectExchangeRates);
         saveBatch(currencyRates);
         return CommonResult.success();
     }
 
     @Override
+    @Transactional
     public Boolean editCurrencyRate(EditCurrencyRateForm form) {
         CurrencyRate currencyRate = new CurrencyRate();
         currencyRate.setId(form.getId());
         currencyRate.setExchangeRate(form.getExchangeRate());//只允许改汇率
+
+        CurrencyRate tmp = this.getById(form.getId());
+
+        //同一个币种同一个月份只允许配置一条
+        QueryWrapper<CurrencyRate> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("month", tmp.getMonth());
+
+        //查询这个月份汇率管理
+        Map<String, CurrencyRate> map = this.baseMapper.selectList(queryWrapper).stream().collect(Collectors.toMap(e -> e.getOcid() + "-" + e.getDcid(), e -> e));
+
+        //查询币种
+        Map<Long, InitComboxStrVO> currencyMap = this.omsClient.initCurrencyInfo().getData().stream().collect(Collectors.toMap(InitComboxStrVO::getId, e -> e));
+        //原币兑人民币才有间接汇率:算法1/原币兑人民币汇率
+        if ("CNY".equals(currencyMap.get(tmp.getDcid()).getCode())) {
+            CurrencyRate tmpRate = map.get(tmp.getDcid() + "-" + tmp.getOcid());
+            BigDecimal indirectExchangeRate = new BigDecimal(1).divide(form.getExchangeRate(), 4, RoundingMode.HALF_EVEN);
+            CurrencyRate indirectRate = new CurrencyRate().setMonth(tmp.getMonth()).setExchangeRate(indirectExchangeRate)
+                    .setOcid(tmp.getDcid()).setDcid(tmp.getOcid());
+            if (tmpRate != null) {
+                indirectRate.setId(tmpRate.getId());
+            } else {
+                indirectRate.setCreatedTime(LocalDateTime.now()).setCreatedUser(UserOperator.getToken());
+            }
+            //同步间接汇率
+            this.saveOrUpdate(indirectRate);
+        }
         return updateById(currencyRate);
     }
 

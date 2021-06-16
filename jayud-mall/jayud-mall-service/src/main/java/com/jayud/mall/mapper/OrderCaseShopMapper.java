0f6b96441a6750472cd23018a807423d16667aff
mfcfine <mfcfine@163.com>
2021-06-16 11:51:23 +0800

    订单箱号对应商品信息

---
+++ jayud-mall/jayud-mall-service/src/main/java/com/jayud/mall/mapper/OrderCaseShopMapper.java
@@ -0,0 +1,16 @@
+package com.jayud.mall.mapper;
+
+import com.jayud.mall.model.po.OrderCaseShop;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 订单箱号对应商品信息 Mapper 接口
+ * </p>
+ *
+ * @author fachang.mao
+ * @since 2021-06-16
+ */
+public interface OrderCaseShopMapper extends BaseMapper<OrderCaseShop> {
+
+}
---
+++ jayud-mall/jayud-mall-service/src/main/java/com/jayud/mall/service/impl/OrderCaseShopServiceImpl.java
@@ -0,0 +1,20 @@
+package com.jayud.mall.service.impl;
+
+import com.jayud.mall.model.po.OrderCaseShop;
+import com.jayud.mall.mapper.OrderCaseShopMapper;
+import com.jayud.mall.service.IOrderCaseShopService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 订单箱号对应商品信息 服务实现类
+ * </p>
+ *
+ * @author fachang.mao
+ * @since 2021-06-16
+ */
+@Service
+public class OrderCaseShopServiceImpl extends ServiceImpl<OrderCaseShopMapper, OrderCaseShop> implements IOrderCaseShopService {
+
+}

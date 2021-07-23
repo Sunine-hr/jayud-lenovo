39264a680fcf27c726834c6811ed948e51f950ad
李达荣 <365165904@qq.com>
2021-07-23 17:49:43 +0800

    客户统计

---
+++ jayud-oms/jayud-oms-web/src/main/java/com/jayud/oms/controller/StatisticalReportController.java
@@ -0,0 +1,16 @@
+package com.jayud.oms.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api("统计报表")
+@RequestMapping("/statisticalReport")
+public class StatisticalReportController {
+
+    /**
+     * 客服待处理统计
+     */
+
+}

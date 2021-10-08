353011b7b585f833c8461d3f4b68b95b402d7c7b
mfcfine <mfcfine@163.com>
2021-10-08 10:35:17 +0800

    云报关-金蝶关系对应，与公司对应(客户|供应商)

--- jayud-finance/jayud-finance-service/src/main/java/com/jayud/finance/service/ICustomsFinanceCoRelationService.java
+++ jayud-finance/jayud-finance-service/src/main/java/com/jayud/finance/service/ICustomsFinanceCoRelationService.java
@@ -3,6 +3,9 @@ package com.jayud.finance.service;
 import com.jayud.finance.po.CustomsFinanceCoRelation;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * <p>
  * 云报关-财务金蝶中的供应商/客户公司名称对应关系表 服务类
@@ -12,5 +15,34 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2020-09-24
  */
 public interface ICustomsFinanceCoRelationService extends IService<CustomsFinanceCoRelation> {
+
+    /**
+     * 查询
+     */
+    List<CustomsFinanceCoRelation> list(Map<String, Object> para);
+
+    /**
+     * 保存
+     */
+    void saveCustomsFinanceCoRelation(CustomsFinanceCoRelation customsFinanceCoRelation);
+
+    /**
+     * 清理redis数据
+     */
+    void clearCompanyRelationMap();
+
+    /**
+     * 刷新
+     * @return
+     */
+    public Map<String, CustomsFinanceCoRelation> refreshCompanyRelationMap();
+
+    /**
+     * 获取
+     * @return
+     */
+    public Map<String, CustomsFinanceCoRelation> getCompanyRelationMap();
+
+
 
 }

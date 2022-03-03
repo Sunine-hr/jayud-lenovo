ec93a49182b81a1453d322568b75255bb71101f4
李达荣 <365165904@qq.com>
2022-03-03 09:43:19 +0800

    对外接口

---
+++ jayud-auth/jayud-auth-web/src/main/java/com/jayud/auth/controller/ExternalApiController.java
@@ -0,0 +1,16 @@
+package com.jayud.auth.controller;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@Api(tags = "auth对外接口")
+@RequestMapping("/api")
+@Slf4j
+public class ExternalApiController {
+
+
+}

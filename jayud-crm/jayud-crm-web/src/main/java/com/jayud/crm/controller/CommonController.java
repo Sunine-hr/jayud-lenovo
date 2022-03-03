55d33f0ccc9eb03635f25cce3504ae35fd89ebd3
李达荣 <365165904@qq.com>
2022-03-03 10:16:08 +0800

    查询业务类型

--- jayud-crm/jayud-crm-web/src/main/java/com/jayud/crm/controller/CommonController.java
+++ jayud-crm/jayud-crm-web/src/main/java/com/jayud/crm/controller/CommonController.java
@@ -1,9 +1,15 @@
 package com.jayud.crm.controller;
 
 
+import com.jayud.auth.model.po.SysDepart;
+import com.jayud.auth.model.po.SysDictItem;
+import com.jayud.common.BaseResult;
 import com.jayud.common.CommonResult;
 import com.jayud.common.entity.InitComboxStrVO;
+import com.jayud.crm.feign.AuthClient;
 import com.jayud.crm.feign.OmsClient;
+import com.jayud.crm.feign.SysDictClient;
+import com.jayud.crm.model.constant.CrmDictCode;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +29,10 @@ public class CommonController {
 
     @Autowired
     private OmsClient omsClient;
+    @Autowired
+    private AuthClient authClient;
+    @Autowired
+    private SysDictClient sysDictClient;
 
     /**
      * 币种
@@ -35,6 +45,27 @@ public class CommonController {
         return CommonResult.success(omsClient.initCurrencyInfo().getData());
     }
 
+
+    /**
+     * @description 查询法人主体
+     **/
+    @ApiOperation("查询法人主体")
+    @RequestMapping("/getLegalEntity")
+    public BaseResult<List<SysDepart>> getLegalEntity() {
+        return authClient.getLegalEntity();
+    }
+
+
+    /**
+     * @description 获取业务类型
+     **/
+    @ApiOperation("获取业务类型")
+    @RequestMapping("/getBusinessType")
+    public BaseResult<List<SysDictItem>> getBusinessType() {
+        //客户管理-业务类型
+        BaseResult<List<SysDictItem>> custBusinessType = sysDictClient.selectItemByDictCode(CrmDictCode.CUST_BUSINESS_TYPE);
+        return custBusinessType;
+    }
 
 }
 

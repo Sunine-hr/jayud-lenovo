1d49390d2c3171627e24f742f646fe33263f8c85
liaolingjie <lljArcher520@163.com>
2022-03-08 09:41:50 +0800

    审核方法修改，系统审核配置修改

--- jayud-auth/jayud-auth-service/src/main/java/com/jayud/auth/service/impl/BPublicCheckServiceImpl.java
+++ jayud-auth/jayud-auth-service/src/main/java/com/jayud/auth/service/impl/BPublicCheckServiceImpl.java
@@ -82,9 +82,11 @@ public class BPublicCheckServiceImpl extends ServiceImpl<BPublicCheckMapper, BPu
         BNoRule bNoRule = bNoRuleService.getNoRulesBySheetCode(checkForm.getSheetCode());
         String checkTable = bNoRule.getCheckTable();
 
+        //开始时间
+        long begin = System.currentTimeMillis();
+
         //获取当前登录用户
         SysUserVO systemUserByName = sysUserService.getSystemUserByName(CurrentUserUtil.getUsername());
-
         //根据id获取当前记录
         Map<String,Object> map = new HashMap<>();
         if(null == bNoRule.getCheckDatabase()){
@@ -94,9 +96,18 @@ public class BPublicCheckServiceImpl extends ServiceImpl<BPublicCheckMapper, BPu
         }
 
         //判断当前单据审核状态  s_level <= s_step  已审核完成
-        Integer sLevel = (Integer)map.get("sLevel");
-        Integer sStep = (Integer)map.get("sStep");
+        Integer sLevel = (Integer)map.get("f_level");
+        Integer sStep = (Integer)map.get("f_step");
         Integer newStep = sStep + 1;
+
+        //定义审核状态
+        String checkFlag;
+        if(newStep.equals(sLevel)){
+            checkFlag = "Y";
+        }else{
+            checkFlag = "N"+newStep;
+        }
+
         if(sLevel <= sStep){
             return BaseResult.error(444,"该订单已审核完成");
         }
@@ -124,44 +135,73 @@ public class BPublicCheckServiceImpl extends ServiceImpl<BPublicCheckMapper, BPu
             }
         }
 
-        //执行审核
-        int update;
-        String checkFlag;
-        if(newStep.equals(sLevel)){
-            checkFlag = "Y";
+        if(checkForm.getCheck()){
+            //执行审核
+            int update;
+            if(null == bNoRule.getCheckDatabase()){
+                update = bPublicCheckMapper.updateData(checkForm.getRecordId(),checkTable,newStep,checkFlag,new Date(),systemUserByName.getName());
+            }else{
+                update = bPublicCheckMapper.updateCheckData(checkForm.getRecordId(),checkTable,bNoRule.getCheckDatabase(),newStep,checkFlag,new Date(),systemUserByName.getName());
+            }
+            if(update <= 0){
+                return BaseResult.error(444,"审核失败，修改订单审核状态失败");
+            }
+
+            //结束时间
+            long end = System.currentTimeMillis();
+
+            //添加审核记录
+            BPublicCheck bPublicCheck = new BPublicCheck();
+            bPublicCheck.setSheetCode(checkForm.getSheetCode());
+            bPublicCheck.setCheckFlag(1);
+            bPublicCheck.setRecordId(checkForm.getRecordId());
+            bPublicCheck.setFLevel(sLevel);
+            bPublicCheck.setFStep(newStep);
+            bPublicCheck.setIsCheck(checkForm.getCheck());
+            bPublicCheck.setTimeConsuming(end-begin);
+            bPublicCheck.setCheckStateFlag(checkFlag);
+            bPublicCheck.setFCheckId(systemUserByName.getId());
+            bPublicCheck.setCreateBy(systemUserByName.getName());
+            bPublicCheck.setCreateTime(new Date());
+            boolean save = this.save(bPublicCheck);
+            if(!save){
+                return BaseResult.error(444,"审核失败，添加审核数据失败");
+            }
+            log.warn("审核成功");
+            return BaseResult.ok("审核成功");
         }else{
-            checkFlag = "N"+sStep;
+            //结束时间
+            long end = System.currentTimeMillis();
+            //添加审核记录
+            BPublicCheck bPublicCheck = new BPublicCheck();
+            bPublicCheck.setSheetCode(checkForm.getSheetCode());
+            bPublicCheck.setCheckFlag(1);
+            bPublicCheck.setRecordId(checkForm.getRecordId());
+            bPublicCheck.setFLevel(sLevel);
+            bPublicCheck.setFStep(newStep);
+            bPublicCheck.setRemark(checkForm.getCheckRemark());
+            bPublicCheck.setCheckStateFlag(checkFlag);
+            bPublicCheck.setIsCheck(checkForm.getCheck());
+            bPublicCheck.setTimeConsuming(end-begin);
+            bPublicCheck.setFCheckId(systemUserByName.getId());
+            bPublicCheck.setCreateBy(systemUserByName.getName());
+            bPublicCheck.setCreateTime(new Date());
+            boolean save = this.save(bPublicCheck);
+            if(!save){
+                return BaseResult.error(444,"审核失败，添加审核数据失败");
+            }
+            log.warn("审核成功");
+            return BaseResult.ok("审核成功");
         }
 
-        if(null == bNoRule.getCheckDatabase()){
-            update = bPublicCheckMapper.updateData(checkForm.getRecordId(),checkTable,newStep,checkFlag,systemUserByName.getId(),LocalDateTime.now(),systemUserByName.getName());
-        }else{
-            update = bPublicCheckMapper.updateCheckData(checkForm.getRecordId(),checkTable,bNoRule.getCheckDatabase(),newStep,checkFlag,systemUserByName.getId(),LocalDateTime.now(),systemUserByName.getName());
-        }
-        if(update != 0){
-            return BaseResult.error(444,"审核失败，修改订单审核状态失败");
-        }
 
-        //添加审核记录
-        BPublicCheck bPublicCheck = new BPublicCheck();
-        bPublicCheck.setSheetCode(checkForm.getSheetCode());
-        bPublicCheck.setCheckFlag(1);
-        bPublicCheck.setRecordId(checkForm.getRecordId());
-        bPublicCheck.setFLevel(sLevel);
-        bPublicCheck.setFStep(newStep);
-        bPublicCheck.setFCheckId(systemUserByName.getId());
-        bPublicCheck.setCreateBy(systemUserByName.getName());
-        bPublicCheck.setCreateTime(new Date());
-        boolean save = this.save(bPublicCheck);
-        if(!save){
-            return BaseResult.error(444,"审核失败，添加审核数据失败");
-        }
-        log.warn("审核成功");
-        return BaseResult.ok("审核成功");
     }
 
     @Override
     public BaseResult unCheck(CheckForm checkForm) {
+        //开始时间
+        long begin = System.currentTimeMillis();
+
         //根据当前单据编号获取表名、库名
         BNoRule bNoRule = bNoRuleService.getNoRulesBySheetCode(checkForm.getSheetCode());
         String checkTable = bNoRule.getCheckTable();
@@ -177,9 +217,19 @@ public class BPublicCheckServiceImpl extends ServiceImpl<BPublicCheckMapper, BPu
             map  = bPublicCheckMapper.getCheckData(checkForm.getRecordId(),checkTable,bNoRule.getCheckDatabase());
         }
         //判断当前审核步长  为0 当前订单未审核
-        Integer sLevel = (Integer)map.get("sLevel");
-        Integer sStep = (Integer)map.get("sStep");
-        Integer newStep = sStep - 1;
+        Integer sLevel = (Integer)map.get("f_level");
+        Integer sStep = (Integer)map.get("f_step");
+        Integer newStep = 0;
+        //定义审核状态
+        String checkFlag = "";
+        if(0 == bNoRule.getDeApprovalSetting()){
+            sStep = 0;
+            checkFlag = "N0";
+        }else{
+            newStep = sStep - 1;
+            checkFlag = "N"+newStep;
+        }
+
         if(sStep != 0){
             return BaseResult.error(444,"该订单还未审核，无法反审");
         }
@@ -208,15 +258,17 @@ public class BPublicCheckServiceImpl extends ServiceImpl<BPublicCheckMapper, BPu
         }
         //执行反审核
         int update;
-        String checkFlag = "N"+sStep;
+
         if(null == bNoRule.getCheckDatabase()){
-            update  = bPublicCheckMapper.updateData(checkForm.getRecordId(),checkTable,newStep,checkFlag,systemUserByName.getId(),LocalDateTime.now(),systemUserByName.getName());
+            update  = bPublicCheckMapper.updateData(checkForm.getRecordId(),checkTable,newStep,checkFlag,new Date(),systemUserByName.getName());
         }else{
-            update   = bPublicCheckMapper.updateCheckData(checkForm.getRecordId(),checkTable,bNoRule.getCheckDatabase(),newStep,checkFlag,systemUserByName.getId(),LocalDateTime.now(),systemUserByName.getName());
+            update   = bPublicCheckMapper.updateCheckData(checkForm.getRecordId(),checkTable,bNoRule.getCheckDatabase(),newStep,checkFlag,new Date(),systemUserByName.getName());
         }
         if(update != 0){
             return BaseResult.error(444,"反审核失败，修改订单审核状态失败");
         }
+        //结束时间
+        long end = System.currentTimeMillis();
 
         //添加反审核记录
         BPublicCheck bPublicCheck = new BPublicCheck();
@@ -225,6 +277,9 @@ public class BPublicCheckServiceImpl extends ServiceImpl<BPublicCheckMapper, BPu
         bPublicCheck.setRecordId(checkForm.getRecordId());
         bPublicCheck.setFLevel(sLevel);
         bPublicCheck.setFStep(newStep);
+        bPublicCheck.setTimeConsuming(end-begin);
+        bPublicCheck.setCheckStateFlag(checkFlag);
+        bPublicCheck.setRemark("第 "+newStep+"级反审核,审核成功");
         bPublicCheck.setFCheckId(systemUserByName.getId());
         bPublicCheck.setFCheckId(systemUserByName.getId());
         bPublicCheck.setCreateBy(systemUserByName.getName());

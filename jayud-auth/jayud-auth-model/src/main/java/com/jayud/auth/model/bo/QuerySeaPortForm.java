60ee7b7bb11d4ea133abb704e5c0c63b92830e17
liaolingjie <lljArcher520@163.com>
2022-03-11 13:53:22 +0800

    海运港口基础数据维护

---
+++ jayud-auth/jayud-auth-model/src/main/java/com/jayud/auth/model/bo/QuerySeaPortForm.java
@@ -0,0 +1,23 @@
+package com.jayud.auth.model.bo;
+
+import com.jayud.common.entity.BasePageForm;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+/**
+ * <p>
+ * 海运订单列表
+ * </p>
+ *
+ * @author
+ * @since
+ */
+@Data
+public class QuerySeaPortForm extends BasePageForm {
+
+    @ApiModelProperty(value = "港口编码")
+    private String code;
+
+    @ApiModelProperty(value = "港口名称")
+    private String name;
+
+}
--- jayud-auth/jayud-auth-service/src/main/java/com/jayud/auth/mapper/SeaPortMapper.java
+++ jayud-auth/jayud-auth-service/src/main/java/com/jayud/auth/mapper/SeaPortMapper.java
@@ -1,24 +1,24 @@
-//package com.jayud.auth.mapper;
-//
-//import com.baomidou.mybatisplus.core.metadata.IPage;
-//import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-//import com.jayud.oceanship.bo.QuerySeaPortForm;
-//import com.jayud.oceanship.po.SeaPort;
-//import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-//import com.jayud.oceanship.vo.SeaPortVO;
-//import org.apache.ibatis.annotations.Mapper;
-//import org.apache.ibatis.annotations.Param;
-//
-///**
-// * <p>
-// * 船港口地址表 Mapper 接口
-// * </p>
-// *
-// * @author LLJ
-// * @since 2021-01-29
-// */
-//@Mapper
-//public interface SeaPortMapper extends BaseMapper<SeaPort> {
-//
-//    IPage<SeaPortVO> findByPage(@Param("page") Page<SeaPortVO> page, @Param("form") QuerySeaPortForm form);
-//}
+package com.jayud.auth.mapper;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jayud.auth.model.bo.QuerySeaPortForm;
+import com.jayud.auth.model.po.SeaPort;
+import com.jayud.auth.model.vo.SeaPortVO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * <p>
+ * 船港口地址表 Mapper 接口
+ * </p>
+ *
+ * @author LLJ
+ * @since 2021-01-29
+ */
+@Mapper
+public interface SeaPortMapper extends BaseMapper<SeaPort> {
+
+    IPage<SeaPortVO> findByPage(@Param("page") Page<SeaPortVO> page, @Param("form") QuerySeaPortForm form);
+}

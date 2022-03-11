273b3b849500be71be1f9c713e818796f50c13cb
李达荣 <365165904@qq.com>
2022-03-11 09:23:04 +0800

    更新

--- jayud-tms/jayud-tms-web/src/main/java/com.jayud.tms/JayudTmsApplication.java
+++ jayud-tms/jayud-tms-web/src/main/java/com.jayud.tms/JayudTmsApplication.java
@@ -1,5 +1,6 @@
 package com.jayud.tms;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,6 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * jayud-tms模块启动类
  */
 @SpringBootApplication(scanBasePackages = "com.jayud")
+@MapperScan(basePackages = {"com.jayud.**.mapper"})
 @EnableDiscoveryClient
 @EnableFeignClients
 public class JayudTmsApplication {
